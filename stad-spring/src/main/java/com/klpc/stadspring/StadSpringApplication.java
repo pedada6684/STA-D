a9f9afea70ebc09d28cbfa7894674943df817e0b
이태경 <korea9300@naver.com>
2024-04-12 17:16:43 +0900

    build: stad-spring 생성

---
+++ stad-spring/src/main/java/com/klpc/stadspring/StadSpringApplication.java
@@ -0,0 +1,13 @@
+package com.klpc.stadspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StadSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StadSpringApplication.class, args);
+	}
+
+}
