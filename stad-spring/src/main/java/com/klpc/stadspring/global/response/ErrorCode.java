fc3e9b426d8baede3432ddb8b4aa7f186df716f0
이태경 <korea9300@naver.com>
2024-04-13 12:29:05 +0900

    feat: responseEntity custom

---
+++ stad-spring/src/main/java/com/klpc/stadspring/global/response/ErrorCode.java
@@ -0,0 +1,37 @@
+package com.klpc.stadspring.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    /* 예시 필요한 것 추가해서 사용*/
+    TEST_NOT_FOUND(HttpStatus.NOT_FOUND, "전달할 메시지"),
+
+    /* S3 업로드 실패 */
+    FAIL_TO_UPLOAD_S3(HttpStatus.BAD_GATEWAY, "S3 서버의 업로드를 실패했습니다."),
+
+    /* JWT Token Error */
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰"),
+    INVALID_TOKEN(HttpStatus.FORBIDDEN, "잘못된 토큰"),
+    TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "토큰을 찾을 수 없습니다."),
+
+    /* JSON Parsing 실패 */
+    JSON_PARSE_IMPOSSIBLE(HttpStatus.BAD_GATEWAY, "API Parsing 중 오류가 발생했습니다."),
+
+    REQUEST_NOT_FOUND(HttpStatus.BAD_REQUEST, "요청 변수를 확인해주십시오."),
+
+    /* entity 존재하지 않음 */
+    ENTITIY_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 정보를 찾을 수 없습니다."),
+
+    /* entity 이미 존재 */
+    ENTITIY_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 존재하는 정보입니다."),
+
+    /* URI */
+    URI_SYNTAX_ERROR(HttpStatus.BAD_GATEWAY, "외부 API 호출 중 오류가 발생했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
