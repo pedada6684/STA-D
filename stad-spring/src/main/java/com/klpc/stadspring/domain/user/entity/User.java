f5a6dc12a6771e704f782f347f97d0a3bbe63ea0
이태경 <korea9300@naver.com>
2024-04-15 16:19:30 +0900

    feat: UserController.withdrawUser 추가

--- stad-spring/src/main/java/com/klpc/stadspring/domain/user/entity/User.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/user/entity/User.java
@@ -23,8 +23,12 @@ public class User {
   private String name;
   private String profile = null;
   private Long type;
+  private Long status; // 탈퇴 0, 정상 1
   private LocalDateTime regDate;
   private LocalDateTime delDate;
+  private String company;
+  private String department;
+  private String comNo;
 
   public static User createNewUser(
           String email,
@@ -39,7 +43,13 @@ public class User {
     user.nickname = nickname;
     user.name = username;
     user.type = type;
+    user.status = 1L;
     user.regDate = LocalDateTime.now();
     return user;
+  }
+
+  public void withdraw(){
+    this.status = 0L;
+    this.delDate = LocalDateTime.now();
   }
 }
