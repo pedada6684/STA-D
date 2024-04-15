f4b53b1c0c3e0edc8890d8316862dc385c2e5d31
이태경 <korea9300@naver.com>
2024-04-15 12:50:48 +0900

    feat: UserController 추가
    상세정보, 프로필 이미지 변경 메서드 추가

---
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/user/service/UserService.java
@@ -0,0 +1,49 @@
+package com.klpc.stadspring.domain.user.service;
+
+import com.klpc.stadspring.domain.user.entity.User;
+import com.klpc.stadspring.domain.user.repository.UserRepository;
+import com.klpc.stadspring.domain.user.service.command.FindMemberByIdCommand;
+import com.klpc.stadspring.domain.user.service.command.UpdateProfileImgCommand;
+import com.klpc.stadspring.global.response.ErrorCode;
+import com.klpc.stadspring.global.response.exception.CustomException;
+import com.klpc.stadspring.util.S3Util;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.net.URL;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional(readOnly = true)
+public class UserService {
+
+  private final UserRepository userRepository;
+  private final S3Util s3Util;
+
+
+  public User findMemberById(FindMemberByIdCommand command){
+    User user = userRepository.findById(command.getId())
+            .orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
+    return user;
+  }
+
+
+  /**
+   * 프로필 사진 변경 메서드
+   * @param command userId, File
+   * @return S3 url
+   */
+  @Transactional(readOnly = false)
+  public String updateProfileImg(UpdateProfileImgCommand command) {
+    log.info("UpdateProfileImgCommand: "+command);
+    User user = userRepository.findById(command.getMemberId())
+            .orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
+    URL S3Url = s3Util.uploadImageToS3(command.getProfileImg(), "user_profile", user.getId().toString());
+    Objects.requireNonNull(S3Url);
+    return S3Url.toString();
+  }
+}
