c10a8704150b5e45e9ef2d80f96b548de2743c4e
이태경 <korea9300@naver.com>
2024-04-15 09:55:09 +0900

    feat: 사용자 정의 exception example 추가

---
+++ stad-spring/src/main/java/com/klpc/stadspring/global/response/exception/ExampleException.java
@@ -0,0 +1,7 @@
+package com.klpc.stadspring.global.response.exception;
+
+public class ExampleException extends BusinessException {
+    public ExampleException(String message) {
+        super(message);
+    }
+}
