bf33f7fa9b0420f830c9a42923bb5d8c0f6b4a77
김은지 <eungg0214@naver.com>
2024-04-16 16:26:55 +0900

    feat: 내가 본 콘텐츠 캐러셀 구현

--- stad/lib/screen/myStad/mycontents_screen.dart
+++ stad/lib/screen/myStad/mycontents_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stad/component/app_bar.dart';
 import 'package:stad/constant/colors.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 
 class MyContentsScreen extends StatefulWidget {
   const MyContentsScreen({super.key});
@@ -10,6 +11,13 @@ class MyContentsScreen extends StatefulWidget {
 }
 
 class _MyContentsScreenState extends State<MyContentsScreen> {
+  final List<String> imageList = [
+    'assets/image/1.png',
+    'assets/image/2.png',
+    'assets/image/3.png',
+    'assets/image/4.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +26,47 @@ class _MyContentsScreenState extends State<MyContentsScreen> {
         titleStyle: TextStyle(
             fontSize: 18, color: mainWhite, fontWeight: FontWeight.bold),
         showBackButton: true,
+      ),
+      body: Center(
+        child: FlutterCarousel(
+          options: CarouselOptions(
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 3),
+            enlargeCenterPage: true,
+            // 중앙 이미지 확대 옵션 활성화
+            height: MediaQuery.of(context).size.height * 0.3,
+            viewportFraction: 0.8,
+            indicatorMargin: 12.0,
+            enableInfiniteScroll: true,
+            slideIndicator: CircularSlideIndicator(), // 페이지 인디케이터 사용
+          ),
+          items: imageList.map((item) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(item),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Image $item',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }).toList(),
+        ),
       ),
     );
   }
