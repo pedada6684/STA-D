eacd145f651b7f2b917770a46031f5104392e3bf
이태경 <korea9300@naver.com>
2024-04-17 15:04:06 +0900

    feat: UserYoutubeInfo 추가

--- stad-spring/src/main/java/com/klpc/stadspring/global/auth/controller/request/AppLoginRequest.java
+++ stad-spring/src/main/java/com/klpc/stadspring/global/auth/controller/request/AppLoginRequest.java
@@ -7,17 +7,19 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class AppLoginRequest {
-    String nickname;
-    String name;
     String email;
-    String profileImage;
+    String phone;
+    String nickname;
+    String profile;
+    String googleAT;
 
     public AppLoginCommand toCommand(){
         return AppLoginCommand.builder()
-                .nickname(nickname)
-                .name(name)
                 .email(email)
-                .profileImage(profileImage)
+                .phone(phone)
+                .nickname(nickname)
+                .profile(profile)
+                .googleAT(googleAT)
                 .build();
     }
 }
