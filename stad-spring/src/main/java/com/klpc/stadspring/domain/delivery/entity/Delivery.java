86b45d4cbff51e5712bbdefb726d646a94c331f1
imin <lllgee@naver.com>
2024-04-17 12:28:30 +0900

    feat : Delivery Entity, Repository 추가

---
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/delivery/entity/Delivery.java
@@ -0,0 +1,20 @@
+package com.klpc.stadspring.domain.delivery.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
