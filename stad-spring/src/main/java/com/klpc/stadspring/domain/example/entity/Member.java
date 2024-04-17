80a841b3c2deda963d142e78a9dd7293bd17f8d4
이태경 <korea9300@naver.com>
2024-04-17 09:15:55 +0900

    style: 탭간격 수정

--- stad-spring/src/main/java/com/klpc/stadspring/domain/example/entity/Member.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/example/entity/Member.java
@@ -12,39 +12,38 @@ import java.time.LocalDateTime;
 @Table(name = "member")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String nickname;
+    private String username;
+    private String profileUrl = null;
+    private Long role;
+    private LocalDateTime regDate;
+    private Long status;
+    private LocalDateTime delDate;
+  //
+  //  @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+  //  private List<BookmarkPlace> bookmarkPlaces;
 
-  @Id
-  @GeneratedValue
-  @Column(name = "member_id")
-  private Long id;
-  @Column(unique = true)
-  private String email;
-  private String password;
-  private String nickname;
-  private String username;
-  private String profileUrl = null;
-  private Long role;
-  private LocalDateTime regDate;
-  private Long status;
-  private LocalDateTime delDate;
-//
-//  @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
-//  private List<BookmarkPlace> bookmarkPlaces;
 
-
-  public static Member createNewMember(
-          String email,
-          String password,
-          String nickname,
-          String username
-  ){
-    Member member = new Member();
-    member.email = email;
-    member.password = password;
-    member.nickname = nickname;
-    member.username = username;
-    member.status = 1L;
-    member.regDate = LocalDateTime.now();
-    return member;
-  }
+    public static Member createNewMember(
+            String email,
+            String password,
+            String nickname,
+            String username
+    ){
+        Member member = new Member();
+        member.email = email;
+        member.password = password;
+        member.nickname = nickname;
+        member.username = username;
+        member.status = 1L;
+        member.regDate = LocalDateTime.now();
+        return member;
+    }
 }
