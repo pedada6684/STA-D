41c2eaec933b874a168b5724519cc0e06865f527
최은희 <gilukji226@naver.com>
2024-04-18 14:17:16 +0900

    feat: Content 엔티티 생성

---
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/bookmark/entity/BookmarkedContent.java
@@ -0,0 +1,32 @@
+package com.klpc.stadspring.domain.contents.bookmark.entity;
+
+import com.klpc.stadspring.domain.contents.detail.entity.ContentDetail;
+import com.klpc.stadspring.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BookmarkedContent {
+    @Id
+    @Column(name = "watched_content_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @Column(name = "content_detail_id")
+    private ContentDetail contentDetail;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+//    @ManyToOne
+//    @Column(name = "user_id")
+//    private User user;
+}
---
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/watched/entity/WatchedContent.java
@@ -0,0 +1,40 @@
+package com.klpc.stadspring.domain.contents.watched.entity;
+
+import com.klpc.stadspring.domain.contents.detail.entity.ContentDetail;
+import com.klpc.stadspring.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WatchedContent {
+    @Id
+    @Column(name = "watched_content_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @Column(name = "content_detail_id")
+    private ContentDetail contentDetail;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+//    @ManyToOne
+//    @Column(name = "user_id")
+//    private User user;
+
+    private boolean status;
+
+    @Column(name = "stop_time")
+    private String stopTime;
+
+    @Column(name = "watched_date")
+    private String watchedDate;
+}
