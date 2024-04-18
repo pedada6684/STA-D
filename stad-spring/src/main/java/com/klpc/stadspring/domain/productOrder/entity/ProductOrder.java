ddbaafe45ce114edff7e5b76ddc2e4879afe8c10
imin <lllgee@naver.com>
2024-04-18 13:08:50 +0900

    feat : ProductOrder 엔티티 수정

--- stad-spring/src/main/java/com/klpc/stadspring/domain/productOrder/entity/ProductOrder.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/productOrder/entity/ProductOrder.java
@@ -1,9 +1,15 @@
 package com.klpc.stadspring.domain.productOrder.entity;
 
+import com.klpc.stadspring.domain.delivery.entity.Delivery;
+import com.klpc.stadspring.domain.product.entity.Product;
+import com.klpc.stadspring.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -14,5 +20,26 @@ public class ProductOrder {
     @Column(name = "order_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private LocalDateTime orderDate;
+
+    private Long cnt;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    private Long contentId;
+
+    private Long advertId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "product_order_id")
+    private List<Product> products;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "product_order_id")
+    private Delivery delivery;
 
 }
