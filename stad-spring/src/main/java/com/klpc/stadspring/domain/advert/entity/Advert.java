f15b26c877c810e5542cee25522cfaf3658178f2
imin <lllgee@naver.com>
2024-04-18 13:07:20 +0900

    feat : Advert 엔티티 수정

--- stad-spring/src/main/java/com/klpc/stadspring/domain/advert/entity/Advert.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/advert/entity/Advert.java
@@ -1,12 +1,16 @@
 package com.klpc.stadspring.domain.advert.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.klpc.stadspring.domain.advertVideo.entity.AdvertVideo;
+import com.klpc.stadspring.domain.product.entity.Product;
+import com.klpc.stadspring.domain.selectedContent.entity.SelectedContent;
+import com.klpc.stadspring.domain.user.entity.User;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,4 +21,36 @@ public class Advert {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String title;
+
+    private String description;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    @Enumerated(EnumType.STRING)
+    private AdvertCategory advertCategory;  //PRODUCT, NOTPRODUCT
+
+    @Column(length = 3000)
+    private String fullVideoUrl;
+
+    @Column(length = 3000)
+    private String directVideoUrl;
+
+    @Column(length = 3000)
+    private String directImgUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "advert")
+    private List<AdvertVideo> advertVideos;
+
+    @OneToMany(mappedBy = "advert")
+    private List<SelectedContent> selectedContents;
+
+    @OneToMany(mappedBy = "advert")
+    private List<Product> products;
 }
