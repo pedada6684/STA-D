c2f0a42419ea8f257a73f875416e6a757168676f
imin <lllgee@naver.com>
2024-04-19 14:31:12 +0900

    feat : Advert 추가 기능 구현

--- stad-spring/src/main/java/com/klpc/stadspring/domain/advert/entity/Advert.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/advert/entity/Advert.java
@@ -23,6 +23,7 @@ public class Advert {
 
     private String title;
 
+    @Column(length = 3000)
     private String description;
 
     private LocalDateTime startDate;
@@ -33,13 +34,10 @@ public class Advert {
     private AdvertCategory advertCategory;  //PRODUCT, NOTPRODUCT
 
     @Column(length = 3000)
-    private String fullVideoUrl;
-
-    @Column(length = 3000)
     private String directVideoUrl;
 
     @Column(length = 3000)
-    private String directImgUrl;
+    private String bannerImgUrl;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
@@ -53,4 +51,37 @@ public class Advert {
 
     @OneToMany(mappedBy = "advert")
     private List<Product> products;
+
+    public static Advert createToAdvert(
+            String title,
+            String description,
+            LocalDateTime startDate,
+            LocalDateTime endDate,
+            String advertCategory,
+            String directVideoUrl,
+            String bannerImgUrl,
+            User user
+//            List<AdvertVideo> advertVideos,
+//            List<SelectedContent> selectedContents,
+//            List<Product> products
+            ){
+        Advert advert = new Advert();
+        advert.title=title;
+        advert.description=description;
+        advert.startDate=startDate;
+        advert.endDate=endDate;
+        if(advertCategory.equals("PRODUCT"))
+            advert.advertCategory=AdvertCategory.PRODUCT;
+        else
+            advert.advertCategory=AdvertCategory.NOTPRODUCT;
+        if(advert.directVideoUrl!=null && !advert.directVideoUrl.isBlank())
+            advert.directVideoUrl=directVideoUrl;
+        advert.bannerImgUrl=bannerImgUrl;
+        advert.user=user;
+//        advert.advertVideos=advertVideos;
+//        advert.selectedContents=selectedContents;
+//        advert.products=products;
+
+        return advert;
+    }
 }
