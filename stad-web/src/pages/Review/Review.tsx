23ad36a2294cc1b93c7ce3a688b4dd9d79182680
westyunn <s2yunn@gmail.com>
2024-04-21 18:29:55 +0900

    feat: 리뷰관리 컴포넌트

--- stad-web/src/pages/Review/Review.tsx
+++ stad-web/src/pages/Review/Review.tsx
@@ -1,3 +1,31 @@
+import {
+  SelectReviewGoodsBox,
+  SelectReviewSortBox,
+} from "../../components/Select/SelectBox";
+import styles from "./Review.module.css";
+import ReviewItem from "./ReviewItem";
 export default function Review() {
-  return <div>리뷰 관리</div>;
+  return (
+    <div className={`${styles.container}`}>
+      <div className={styles.title}>리뷰관리</div>
+      <div className={`${styles.subTitle}`}>리뷰</div>
+      <div className={`${styles.reviews}`}>
+        <div className={`${styles.lowReviews} ${styles.category}`}>
+          <div className={`${styles.reviewsText}`}>낮은 별점 리뷰</div>
+          <div className={`${styles.reviewsNum}`}>210</div>
+        </div>
+        <div className={`${styles.recentReviews} ${styles.category}`}>
+          <div className={`${styles.reviewsText}`}>최신 리뷰</div>
+          <div className={`${styles.reviewsNum}`}>21</div>
+        </div>
+      </div>
+      <div className={`${styles.select}`}>
+        <SelectReviewSortBox />
+        <SelectReviewGoodsBox />
+      </div>
+      <div className={`${styles.item}`}>
+        <ReviewItem />
+      </div>
+    </div>
+  );
 }
