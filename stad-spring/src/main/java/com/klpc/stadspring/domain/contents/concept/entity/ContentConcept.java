8e0696286ae8abb330e5cb090b6d93133e4f049f
최은희 <gilukji226@naver.com>
2024-04-22 15:08:58 +0900

    refactor: Column 어노테이션 삭제
    Former-commit-id: b839011aff546478a2f281f44afefa682b3c48f7

--- stad-spring/src/main/java/com/klpc/stadspring/domain/contents/concept/entity/ContentConcept.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/concept/entity/ContentConcept.java
@@ -1,31 +1,20 @@
 package com.klpc.stadspring.domain.contents.concept.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ContentConcept {
     @Id
-    @Column(name = "content_concept_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "is_movie")
     private boolean isMovie;
-
-    private String title;
-
-    @Column(name = "thumbnail_url")
     private String thumbnailUrl;
-
-    @Column(name = "audience_age")
     private String audienceAge;
-
-    private String Creator;
-
-    private String Cast;
+    private String creator;
+    private String cast;
 }
