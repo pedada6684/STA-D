4e1321e18d27e3d8ff11d371ec56af8ebe8dfc1a
최은희 <gilukji226@naver.com>
2024-04-24 13:46:27 +0900

    feat: concept repository
    Former-commit-id: 12f305f4e116e1d3527a57b6a503760b46f40b6f

--- stad-spring/src/main/java/com/klpc/stadspring/domain/contents/concept/repository/ContentConceptRepository.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/concept/repository/ContentConceptRepository.java
@@ -1,4 +1,8 @@
 package com.klpc.stadspring.domain.contents.concept.repository;
 
-public class ContentConceptRepository {
+import com.klpc.stadspring.domain.contents.concept.entity.ContentConcept;
+import com.klpc.stadspring.domain.contents.concept.repository.custom.ContentConceptRepositoryCustom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContentConceptRepository extends JpaRepository<ContentConcept, Long>, ContentConceptRepositoryCustom {
 }
