cf673b6ad00b69f3b6e8a65e6c33c4fb8e25a0eb
최은희 <gilukji226@naver.com>
2024-04-24 13:44:24 +0900

    feat: get content response
    Former-commit-id: f1deca1ae6b8ebcdf2a550002055f52e33ea03d5

---
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/detail/controller/response/GetContentConceptResponse.java
@@ -0,0 +1,35 @@
+package com.klpc.stadspring.domain.contents.detail.controller.response;
+
+import com.klpc.stadspring.domain.contents.concept.entity.ContentConcept;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class GetContentConceptResponse {
+    String title;
+    String thumbnailUrl;
+    String playtime;
+    String releaseYear;
+    String audienceAge;
+    String creator;
+    String cast;
+    String description;
+
+    public static GetContentConceptResponse from(ContentConcept contentConcept) {
+        return GetContentConceptResponse.builder()
+                .title(contentConcept.getTitle())
+                .thumbnailUrl(contentConcept.getThumbnailUrl())
+                .playtime(contentConcept.getPlaytime())
+                .releaseYear(contentConcept.getReleaseYear())
+                .audienceAge(contentConcept.getAudienceAge())
+                .creator(contentConcept.getCreator())
+                .cast(contentConcept.getCast())
+                .description(contentConcept.getDescription())
+                .build();
+    }
+}
