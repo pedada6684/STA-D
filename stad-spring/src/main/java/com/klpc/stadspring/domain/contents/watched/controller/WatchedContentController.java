05708d55ceda93842ab525cbc00326fa41bb555f
최은희 <gilukji226@naver.com>
2024-04-25 20:45:15 +0900

    feat: add, modify watchedContent

---
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/watched/controller/WatchedContentController.java
@@ -0,0 +1,56 @@
+package com.klpc.stadspring.domain.contents.watched.controller;
+
+import com.klpc.stadspring.domain.contents.watched.controller.request.AddWatchingContentRequest;
+import com.klpc.stadspring.domain.contents.watched.controller.request.ModifyWatchingContentRequest;
+import com.klpc.stadspring.domain.contents.watched.controller.response.AddWatchingContentResponse;
+import com.klpc.stadspring.domain.contents.watched.controller.response.ModifyWatchingContentResponse;
+import com.klpc.stadspring.domain.contents.watched.service.WatchedContentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Tag(name = "콘텐츠 카테고리 컨트롤러", description = "Contents Category Controller API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/contents-watch")
+public class WatchedContentController {
+    private final WatchedContentService service;
+
+    @PostMapping("/add")
+    @Operation(summary = "시청 중인 영상 등록", description = "Watched Content Add API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "시청 중인 영상 등록 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 형식"),
+            @ApiResponse(responseCode = "500", description = "내부 서버 오류")
+    })
+    public ResponseEntity<AddWatchingContentResponse> addWatchingContent(@RequestBody AddWatchingContentRequest request) {
+        log.info("AddWatchingContentRequest : "+request);
+
+        try {
+            AddWatchingContentResponse response = service.addWatchingContent(request.toCommand());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PutMapping("/update")
+    @Operation(summary = "시청 중인 영상 등록", description = "Watched Content Add API")
+    public ResponseEntity<ModifyWatchingContentResponse> modifyWatchingContent(@RequestBody ModifyWatchingContentRequest request) {
+        log.info("ModifyWatchingContentRequest : "+request);
+
+        try {
+            ModifyWatchingContentResponse response = service.modifyWatchingContent(request.toCommand());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+}
--- stad-spring/src/main/java/com/klpc/stadspring/domain/contents/watched/service/WatchedContentService.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/watched/service/WatchedContentService.java
@@ -1,23 +1,71 @@
 package com.klpc.stadspring.domain.contents.watched.service;
 
+import com.klpc.stadspring.domain.contents.detail.entity.ContentDetail;
+import com.klpc.stadspring.domain.contents.detail.repository.ContentDetailRepository;
+import com.klpc.stadspring.domain.contents.watched.controller.response.AddWatchingContentResponse;
+import com.klpc.stadspring.domain.contents.watched.controller.response.ModifyWatchingContentResponse;
+import com.klpc.stadspring.domain.contents.watched.entity.WatchedContent;
 import com.klpc.stadspring.domain.contents.watched.repository.WatchedContentRepository;
+import com.klpc.stadspring.domain.contents.watched.service.command.request.AddWatchingContentCommand;
+import com.klpc.stadspring.domain.contents.watched.service.command.request.ModifyWatchingContentCommand;
+import com.klpc.stadspring.domain.user.entity.User;
+import com.klpc.stadspring.domain.user.repository.UserRepository;
 import com.klpc.stadspring.global.response.ErrorCode;
 import com.klpc.stadspring.global.response.exception.CustomException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class WatchedContentService {
-    private final WatchedContentRepository repository;
+    private final WatchedContentRepository watchedContentRepository;
+    private final UserRepository userRepository;
+    private final ContentDetailRepository detailRepository;
 
     public List<Long> getDetailIdByUserId(Long userId) {
-        List<Long> detailIdList = repository.findDetailIdByUserId(userId)
+        List<Long> detailIdList = watchedContentRepository.findDetailIdByUserId(userId)
                 .orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
         return detailIdList;
     }
+
+    public AddWatchingContentResponse addWatchingContent(AddWatchingContentCommand command) {
+        log.info("AddWatchingContentCommand : " + command);
+
+        User user = userRepository.findById(command.getUserId())
+                .orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
+        ContentDetail detail = detailRepository.findById(command.getDetailId())
+                .orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
+
+        WatchedContent newWatchedContent = WatchedContent.createToWatchedContent(
+                detail,
+                user,
+                false,
+                "00:00:00");
+        watchedContentRepository.save(newWatchedContent);
+
+        return AddWatchingContentResponse.builder().result("시청 중인 컨텐츠가 성공적으로 생성되었습니다.").build();
+    }
+
+    public ModifyWatchingContentResponse modifyWatchingContent(ModifyWatchingContentCommand command) {
+        log.info("ModifyWatchingContentCommand : " + command);
+
+        WatchedContent updatedWatchedContent = watchedContentRepository.findByUserIdAndDetailId(command.getUserId(), command.getDetailId())
+                .orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
+        updatedWatchedContent.modifyWatchedContent(
+                command.isStatus(),
+                command.getStopTime()
+        );
+
+        Long result = watchedContentRepository.updateWatchedContent(updatedWatchedContent);
+        if (result == 0)
+            return ModifyWatchingContentResponse.builder().result("**컨텐츠 시청 시간이 저장되지 않았습니다.**").build();
+        return ModifyWatchingContentResponse.builder().result("컨텐츠 시청 시간이 성공적으로 저장되었습니다.").build();
+    }
+
 }
