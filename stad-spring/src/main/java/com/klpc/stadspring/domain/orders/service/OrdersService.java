f2398d61dd6cc0b4c39d559149599487a6f9bbaf
imin <lllgee@naver.com>
2024-04-25 14:57:33 +0900

    feat : OrdersList 목록 조회 기능 추가

--- stad-spring/src/main/java/com/klpc/stadspring/domain/orders/service/OrdersService.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/orders/service/OrdersService.java
@@ -7,9 +7,11 @@ import com.klpc.stadspring.domain.orderProduct.entity.OrderProduct;
 import com.klpc.stadspring.domain.orderProduct.repository.OrderProductRepository;
 import com.klpc.stadspring.domain.orders.controller.request.AddOrdersRequest;
 import com.klpc.stadspring.domain.orders.controller.response.AddOrdersResponse;
+import com.klpc.stadspring.domain.orders.controller.response.GetOrdersListResponse;
 import com.klpc.stadspring.domain.orders.entity.Orders;
 import com.klpc.stadspring.domain.orders.repository.OrdersRepository;
 import com.klpc.stadspring.domain.orders.service.command.request.AddOrderRequestCommand;
+import com.klpc.stadspring.domain.orders.service.command.response.GetOrdersListResponseCommand;
 import com.klpc.stadspring.domain.product.entity.Product;
 import com.klpc.stadspring.domain.product.repository.ProductRepository;
 import com.klpc.stadspring.domain.user.entity.User;
@@ -20,6 +22,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -58,6 +63,44 @@ public class OrdersService {
         deliveryRepository.save(delivery);
 
         return AddOrdersResponse.builder().result("success").build();
+    }
+
+    /**
+     * 주문 목록 조회
+     * @param userId
+     * @return
+     */
+    public GetOrdersListResponse getOrdersList(Long userId){
+        User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
+
+        List<Orders> ordersByUser = ordersRepository.findAllByUser(user);
+        List<GetOrdersListResponseCommand> response = new ArrayList<>();
+        for(Orders orders : ordersByUser){
+            List<Long> productIdList = new ArrayList<>();
+            for(OrderProduct orderProduct : orders.getOrderProducts()){
+                productIdList.add(orderProduct.getProduct().getId());
+            }
+            List<String> productNameList = new ArrayList<>();
+            for(OrderProduct orderProduct : orders.getOrderProducts()){
+                productNameList.add(orderProduct.getProduct().getName());
+            }
+            List<String> productThumbnailUrl = new ArrayList<>();
+            for(OrderProduct orderProduct : orders.getOrderProducts()){
+                productNameList.add(orderProduct.getProduct().getThumbnail());
+            }
+            GetOrdersListResponseCommand command = GetOrdersListResponseCommand.builder()
+                    .ordersId(orders.getId())
+                    .orderDate(orders.getOrderDate().toLocalDate().toString())
+                    .contentId(orders.getContentId())
+                    .advertId(orders.getAdvertId())
+                    .deliveryStatus(orders.getDelivery().getStatus().toString())
+                    .productId(productIdList)
+                    .productName(productNameList)
+                    .productThumbnailUrl(productThumbnailUrl)
+                    .build();
+            response.add(command);
+        }
+        return GetOrdersListResponse.builder().data(response).build();
     }
 
 }
