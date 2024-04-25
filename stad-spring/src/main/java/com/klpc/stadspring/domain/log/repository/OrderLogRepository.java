65a65d30dbded6f28774e41b88435821e0c22ee4
박지운 <zieunpk0924@naver.com>
2024-04-26 01:22:41 +0900

    feat: Log 기준별 조회 구현

--- stad-spring/src/main/java/com/klpc/stadspring/domain/log/repository/OrderLogRepository.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/log/repository/OrderLogRepository.java
@@ -10,4 +10,7 @@ import java.util.Optional;
 public interface OrderLogRepository extends JpaRepository<OrderLog, Long> {
     @Query("SELECT COUNT(o) FROM OrderLog o WHERE o.advertVideoId = :advertVideoId AND o.status = TRUE")
     Optional<Long> countOrderLog(@Param("advertVideoId") Long advertVideoId);
+
+    @Query("SELECT COUNT(o) FROM OrderLog o WHERE o.advertVideoId = :advertVideoId AND o.status = FALSE")
+    Optional<Long> countOrderCancelLog(@Param("advertVideoId") Long advertVideoId);
 }
