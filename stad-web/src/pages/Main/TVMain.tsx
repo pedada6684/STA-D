6204facf37ad3eb42d0fac0d54fd52500069583e
이서윤 <s2yunn@gmail.com>
2024-04-25 17:46:56 +0900

    feat: TV 메인페이지

--- stad-web/src/pages/Main/TVMain.tsx
+++ stad-web/src/pages/Main/TVMain.tsx
@@ -1,3 +1,16 @@
+import MainCarousel from "../../components/Carousel/MainCarousel";
+import RecentWatching from "../../components/Carousel/RecentWatching";
+import TVContainer from "../../components/Container/TVContainer";
+import TVNav from "../../components/Nav/TVNav";
+
 export default function TVMain() {
-  return <div>티비 메인 </div>;
+  return (
+    <div>
+      <TVContainer>
+        <TVNav />
+        <MainCarousel />
+        <RecentWatching />
+      </TVContainer>
+    </div>
+  );
 }
