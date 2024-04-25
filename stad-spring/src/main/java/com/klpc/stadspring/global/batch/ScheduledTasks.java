55fed462edde2d8f3c9d1d9d3daf4f31b0f21459
박지운 <zieunpk0924@naver.com>
2024-04-26 00:30:56 +0900

    feat: AdvertClickCount step 구현
    Former-commit-id: 95e85d5f96d1fd003b0d1020ded702bb3e7db36c

--- stad-spring/src/main/java/com/klpc/stadspring/global/batch/ScheduledTasks.java
+++ stad-spring/src/main/java/com/klpc/stadspring/global/batch/ScheduledTasks.java
@@ -11,18 +11,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledTasks {
-//
+
 //    @Autowired
 //    private JobLauncher jobLauncher;
 //
 //    @Autowired
-//    private Job updateStatsJob;
+//    private Job LogCountJob;
 //
-//    @Scheduled(fixedDelay = 200000)
+//    @Scheduled(fixedDelay = 60000)
 //    /**
 //     * 1시간마다 실행
 //     */
-////    @Scheduled(cron = "0 0 0/1 * * *")
+//    @Scheduled(cron = "0 0 0/1 * * *")
 //    public void runJob() throws JobExecutionException {
 //        JobParameters jobParameters = new JobParametersBuilder()
 //                .addLong("time", System.currentTimeMillis(), true)
--- stad-spring/src/main/java/com/klpc/stadspring/global/batch/config/BatchConfig.java
+++ stad-spring/src/main/java/com/klpc/stadspring/global/batch/config/BatchConfig.java
@@ -1,6 +1,16 @@
 package com.klpc.stadspring.global.batch.config;
 
+import com.klpc.stadspring.domain.advert.repository.AdvertRepository;
 import com.klpc.stadspring.domain.log.entity.AdvertClickLog;
+import com.klpc.stadspring.domain.log.entity.AdvertVideoLog;
+import com.klpc.stadspring.domain.log.repository.AdvertClickLogRepository;
+import com.klpc.stadspring.domain.log.repository.AdvertVideoLogRepository;
+import com.klpc.stadspring.domain.log.repository.OrderLogRepository;
+import com.klpc.stadspring.domain.log.repository.OrderReturnLogRepository;
+import com.klpc.stadspring.global.response.ErrorCode;
+import com.klpc.stadspring.global.response.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -17,50 +27,62 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Sort;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import java.time.LocalDateTime;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+@Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class BatchConfig {
-//    @Bean
-//    public Job LogCountJob(JobRepository jobRepository,
-//                           Step simpleStep1,
-//                           Step simpleStep2
-//    ) {
-//        return new JobBuilder("LogCountJob", jobRepository)
-//                .start(simpleStep1)
-//                .next(simpleStep2)
-//                .build();
-//    }
-//    @Bean
-//    public Step AdvertClickCount(JobRepository jobRepository, Tasklet advertClickCountTasklet, PlatformTransactionManager platformTransactionManager){
-//        return new StepBuilder("advertClickCountStep", jobRepository)
-//                .tasklet(advertClickCountTasklet, platformTransactionManager).build();
-//    }
-//    @Bean
-//    public Tasklet advertClickCountTasklet(){
-//        return ((contribution, chunkContext) -> {
-//            ArrayList<Long> ids = new ArrayList<>(AnnotationBasedAOP.map.keySet());
-//            HashMap<String, Long> names = new HashMap<>();
-//
-//            List<Movie> movieList = movieRepository.findAllById(ids);
-//
-//            for (Movie movie : movieList) {
-//                names.put(movie.getMovieName(), AnnotationBasedAOP.map.get(movie.getId()));
-//            }
-//
-//            // TODO: 2023/06/12 이곳에 movieNameMap을 파일/디비로 저장하는 로직이 필요하다.
-//            //  현재는 로깅하도록 하자
-//
+
+    private final AdvertClickLogRepository advertClickLogRepository;
+    private final AdvertVideoLogRepository advertVideoLogRepository;
+    private final OrderLogRepository orderLogRepository;
+    private final OrderReturnLogRepository orderReturnLogRepository;
+    private final AdvertRepository advertRepository;
+
+    @Bean
+    public Job LogCountJob(JobRepository jobRepository,
+                           Step AdvertClickCount,
+                           Step simpleStep2
+    ) {
+        return new JobBuilder("LogCountJob", jobRepository)
+                .start(AdvertClickCount)
+                .next(simpleStep2)
+                .build();
+    }
+    @Bean
+    public Step AdvertClickCount(JobRepository jobRepository, Tasklet advertClickCountTasklet, PlatformTransactionManager platformTransactionManager){
+        return new StepBuilder("advertClickCountStep", jobRepository)
+                .tasklet(advertClickCountTasklet, platformTransactionManager)
+                .build();
+    }
+    @Bean
+    public Tasklet advertClickCountTasklet(){
+        return ((contribution, chunkContext) -> {
+            ArrayList<Long> advertIdList = new ArrayList<>(advertRepository.findAllAdvertIds());
+            HashMap<Long, Long> clickCountList = new HashMap<>();
+
+            for (Long advertId : advertIdList) {
+                Long clickCount = advertClickLogRepository.countAddClickLogByAdvertId(advertId)
+                        .orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
+
+                clickCountList.put(advertId, clickCount);
+            }
+            log.info("list: "+clickCountList);
+
+            // TODO: 2023/06/12 이곳에 movieNameMap을 파일/디비로 저장하는 로직이 필요하다.
+            //  현재는 로깅하도록 하자
+
 //            for (String s : names.keySet()) {
 //                log.info("영화 이름 :" + s + ", 호출 횟수 :" + names.get(s));
 //            }
-//
-////            AnnotationBasedAOP.map.clear();
-//
-//            return RepeatStatus.FINISHED;
-//        });
-//    }
+
+//            AnnotationBasedAOP.map.clear();
+
+            return RepeatStatus.FINISHED;
+        });
+    }
 //
 //    @Bean
 //    public Step simpleStep2(JobRepository jobRepository, Tasklet testTasklet, PlatformTransactionManager platformTransactionManager){
