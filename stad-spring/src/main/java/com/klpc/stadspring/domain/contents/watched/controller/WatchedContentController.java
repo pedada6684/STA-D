319db9251e7387ce4f3b82bc8af360bf2def8921
최은희 <gilukji226@naver.com>
2024-04-25 21:59:09 +0900

    feat: 시청일 추가
    Former-commit-id: 44a6264b6f9434c66d92227fc806285620d70362

--- stad-spring/src/main/java/com/klpc/stadspring/domain/contents/watched/controller/WatchedContentController.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/watched/controller/WatchedContentController.java
@@ -11,12 +11,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@Tag(name = "콘텐츠 카테고리 컨트롤러", description = "Contents Category Controller API")
+@Tag(name = "시청 콘텐츠 컨트롤러", description = "Watched Contents Controller API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/contents-watch")
@@ -42,7 +41,7 @@ public class WatchedContentController {
     }
 
     @PutMapping("/update")
-    @Operation(summary = "시청 중인 영상 등록", description = "Watched Content Add API")
+    @Operation(summary = "시청 중인 영상 수정", description = "Watched Content Modify API")
     public ResponseEntity<ModifyWatchingContentResponse> modifyWatchingContent(@RequestBody ModifyWatchingContentRequest request) {
         log.info("ModifyWatchingContentRequest : "+request);
 
--- stad-spring/src/main/java/com/klpc/stadspring/domain/contents/watched/service/command/request/ModifyWatchingContentCommand.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/watched/service/command/request/ModifyWatchingContentCommand.java
@@ -3,6 +3,8 @@ package com.klpc.stadspring.domain.contents.watched.service.command.request;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 @Builder
 public class ModifyWatchingContentCommand {
