add2b69487411e20a7b7dc903337a64798d4ece5
imin <lllgee@naver.com>
2024-04-26 10:35:08 +0900

    feat : delivery 삭제

--- stad-spring/src/main/java/com/klpc/stadspring/domain/delivery/repository/DeliveryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.klpc.stadspring.domain.delivery.repository;
-
-import com.klpc.stadspring.domain.delivery.entity.Delivery;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeliveryRepository extends JpaRepository<Delivery,Long> {
-}
