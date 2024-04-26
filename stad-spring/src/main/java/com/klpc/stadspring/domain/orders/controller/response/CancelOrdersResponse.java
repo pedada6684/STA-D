20f509fbea592b4f1fd912a5a829f7dedec9eb63
imin <lllgee@naver.com>
2024-04-26 14:09:44 +0900

    feat : orders 취소 기능 추가

---
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/orders/controller/response/CancelOrdersResponse.java
@@ -0,0 +1,12 @@
+package com.klpc.stadspring.domain.orders.controller.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CancelOrdersResponse {
+
+    String result;
+
+}
