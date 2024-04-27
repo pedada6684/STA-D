93a0b2b09d4f7f50647ce8a29a90f50b64970093
imin <lllgee@naver.com>
2024-04-27 17:13:54 +0900

    fix : cartProduct service controller 수정
    Former-commit-id: 4c43b283114ff23a1a0a57918b50cf10ef160447

--- stad-spring/src/main/java/com/klpc/stadspring/domain/cart/entity/CartProduct.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/cart/entity/CartProduct.java
@@ -1,5 +1,7 @@
 package com.klpc.stadspring.domain.cart.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.klpc.stadspring.domain.product.entity.Product;
 import com.klpc.stadspring.domain.productType.entity.ProductType;
 import com.klpc.stadspring.domain.product_review.entity.ProductReview;
@@ -20,13 +22,13 @@ public class CartProduct {
     @Column(name = "cart_product_id")
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "cart_id")
-    private Cart cart;
-
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_type_id")
     private ProductType productType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @Setter
     @Column(name = "quantity")
@@ -39,15 +41,15 @@ public class CartProduct {
     private Long contentId;
 
     public static CartProduct createNewCartProduct(
-            Cart cart,
             ProductType productType,
+            User user,
             Long quantity,
             Long advertId,
             Long contentId
     ) {
         CartProduct cartProduct = new CartProduct();
-        cartProduct.cart = cart;
         cartProduct.productType = productType;
+        cartProduct.user= user;
         cartProduct.quantity = quantity;
         cartProduct.advertId = advertId;
         cartProduct.contentId = contentId;
