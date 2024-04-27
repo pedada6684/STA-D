e9035db53b8c71a893ed081f9b48410f162688f8
imin <lllgee@naver.com>
2024-04-27 18:25:51 +0900

    feat : 컨텐츠 관련 광고 목록 출력 기능 추가
    Former-commit-id: 81061850a1b89d1bb75c3ef8c3df3a2a51c1336e

--- stad-spring/src/main/java/com/klpc/stadspring/domain/advert/controller/AdvertController.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/advert/controller/AdvertController.java
@@ -30,6 +30,7 @@ public class AdvertController {
     @Operation(summary = "광고 등록", description = "광고 등록")
     @ApiResponse(responseCode = "200", description = "광고가 등록 되었습니다.")
     public ResponseEntity<AddAdvertResponse> addAdvert(@RequestBody AddAdvertRequest request){
+        log.info("광고 등록 Controller"+"\n"+"title : "+request.getTitle());
         AddAdvertRequestCommand command = AddAdvertRequestCommand.builder()
                 .userId(request.getUserId())
                 .title(request.getTitle())
@@ -51,6 +52,7 @@ public class AdvertController {
     @Operation(summary = "광고 수정", description = "광고 수정")
     @ApiResponse(responseCode = "200", description = "광고가 수정 되었습니다.")
     public ResponseEntity<ModifyAdvertResponse> modifyAdvert(@RequestBody ModifyAdvertRequest request){
+        log.info("광고 수정 Controller"+"\n"+"title : "+request.getTitle());
         ModifyAdvertRequestCommand command = ModifyAdvertRequestCommand.builder()
                 .advertId(request.getAdvertId())
                 .title(request.getTitle())
@@ -71,6 +73,7 @@ public class AdvertController {
     @Operation(summary = "광고 삭제", description = "광고 삭제")
     @ApiResponse(responseCode = "200", description = "광고가 삭제 되었습니다.")
     public ResponseEntity<DeleteAdvertResponse> deleteAdvert(@RequestParam("advertId") Long advertId){
+        log.info("광고 삭제 Controller"+"\n"+"advertId : "+advertId);
         DeleteAdvertResponse response = advertService.deleteAdvert(advertId);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
@@ -80,6 +83,7 @@ public class AdvertController {
     @Operation(summary = "광고 조회", description = "광고 조회")
     @ApiResponse(responseCode = "200", description = "광고가 조회 되었습니다.")
     public ResponseEntity<GetAdvertResponse> getAdvert(@RequestParam("advertId") Long advertId){
+        log.info("광고 조회 Controller"+"\n"+"advertId : "+advertId);
         GetAdvertResponse response = advertService.getAdvert(advertId);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
@@ -89,22 +93,28 @@ public class AdvertController {
     @Operation(summary = "광고 리스트 조회", description = "광고 리스트 조회")
     @ApiResponse(responseCode = "200", description = "광고 리스트가 조회 되었습니다.")
     public ResponseEntity<GetAdvertListResponse> getAdvertList(@RequestParam("userId") Long userId){
+        log.info("광고 리스트 조회 Controller"+"\n"+"userId : "+userId);
         GetAdvertListResponse response = advertService.getAdvertList(userId);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-//    @GetMapping("get-list-by-content")
-//    @Operation(summary = "컨텐츠 별 광고 리스트 조회", description = "컨텐츠 별 광고 리스트 조회")
-//    @ApiResponse(responseCode = "200", description = "광고 리스트가 조회 되었습니다.")
-//    public ResponseEntity<GetAdvertListByContentResponse> getAdvertListByContent(@RequestParam("contentId") Long contentId){
-//
-//    }
+    @GetMapping("get-list-by-content")
+    @Operation(summary = "컨텐츠 관련 광고 리스트 조회", description = "컨텐츠 별 광고 리스트 조회")
+    @ApiResponse(responseCode = "200", description = "광고 리스트가 조회 되었습니다.")
+    public ResponseEntity<GetAdvertListByContentResponse> getAdvertListByContent(@RequestParam("contentId") Long contentId){
+        log.info("컨텐츠 관련 광고 리스트 조회 Controller"+"\n"+"contentId : "+contentId);
+        GetAdvertListByContentResponse response = advertService.getAdvertListByContent(contentId);
+
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
 
     @GetMapping("get-list-by-click")
     @Operation(summary = "인기 광고 리스트 조회", description = "인기 광고 리스트 조회")
     @ApiResponse(responseCode = "200", description = "광고 리스트가 조회 되었습니다.")
     public ResponseEntity<GetAdvertListByClickResponse> getAdvertListByClick(){
+        log.info("인기 광고 리스트 조회 Controller"+"\n");
+
         GetAdvertListByClickResponse response = advertService.getAdvertListByClick();
 
         return new ResponseEntity<>(response, HttpStatus.OK);
