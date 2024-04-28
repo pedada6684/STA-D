c12289e0478a6fc050b43cbcdb025d8391cc531c
이서윤 <s2yunn@gmail.com>
2024-04-29 07:45:47 +0900

    feat: 영상 상세 연결

--- stad-web/src/pages/Streaming/VideoList.tsx
+++ stad-web/src/pages/Streaming/VideoList.tsx
@@ -1,13 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./VideoList.module.css";
+import { Video } from "../Category/TVDetail";
 interface VideoListProps {
-  items?: { url: string; title: string }[];
+  items: Video[]; // 올바른 타입 지정
 }
 export default function VideoList({ items = [] }: VideoListProps) {
+  const navigate = useNavigate();
   return (
     <div className={`${styles.gridContainer}`}>
       {items.map((data, index) => (
-        <div className={`${styles.thumbnail}`} key={index}>
-          <img src={data.url} alt="영상 썸네일" />
+        <div
+          className={`${styles.thumbnail}`}
+          key={index}
+          onClick={() => navigate(`/tv/${data.id}`)}
+        >
+          <img src={data.thumbnailUrl} alt="영상 썸네일" />
           <div className={`${styles.title}`}>{data.title}</div>
         </div>
       ))}
