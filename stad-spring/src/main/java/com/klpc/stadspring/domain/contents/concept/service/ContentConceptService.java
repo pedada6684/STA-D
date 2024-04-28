a0cf47774b42ffcbfe6348207596640b82c8e55f
최은희 <gilukji226@naver.com>
2024-04-28 13:25:37 +0900

    feat: create 생성

--- stad-spring/src/main/java/com/klpc/stadspring/domain/contents/concept/service/ContentConceptService.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/concept/service/ContentConceptService.java
@@ -1,6 +1,5 @@
 package com.klpc.stadspring.domain.contents.concept.service;
 
-import com.klpc.stadspring.domain.contents.concept.controller.response.AddConceptResponse;
 import com.klpc.stadspring.domain.contents.concept.entity.ContentConcept;
 import com.klpc.stadspring.domain.contents.concept.repository.ContentConceptRepository;
 import com.klpc.stadspring.domain.contents.concept.service.command.request.AddConceptRequestCommand;
@@ -35,7 +34,7 @@ public class ContentConceptService {
     }
 
     // contentConcept 등록
-    public AddConceptResponse addConcept(AddConceptRequestCommand command) {
+    public void addConcept(AddConceptRequestCommand command) {
         log.info("AddConceptRequestCommand : " + command);
 
         ContentConcept newContentConcept = ContentConcept.createContentConcept(
@@ -49,7 +48,5 @@ public class ContentConceptService {
                 command.getThumbnail(),
                 command.getTitle());
         repository.save(newContentConcept);
-
-        return AddConceptResponse.builder().result("콘텐츠 콘셉트가 성공적으로 등록되었습니다.").build();
     }
 }
