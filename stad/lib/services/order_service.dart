42605aa3e5e348b5234c0bef85a4f90ae79d00b6
김은지 <eungg0214@naver.com>
2024-04-29 22:16:00 +0900

    refactor: 엔드포인트 상수로 설정

--- stad/lib/services/order_service.dart
+++ stad/lib/services/order_service.dart
@@ -1,34 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:stad/constant/api.dart';
 import 'package:stad/models/order_model.dart';
 
 class OrderService {
   final Dio dio = Dio();
-  final String locUrl = 'https://www.mystad.com/api/user/location';
 
   // final String EmUrl = 'http://192.168.31.202:8080/api/user/location';
-  final String orderUrl = 'https://www.mystad.com/api/orders/list?userId=';
-
-  //배송지 추가
-  Future<void> sendAddressData(int userId, String location, String name,
-      String phone, String locationNick) async {
-    try {
-      final response = await dio.post(locUrl, data: {
-        'userId': '1',
-        'location': location,
-        'name': name,
-        'phone': phone,
-        'locationNick': locationNick,
-      });
-      // 응답 처리
-      if (response.statusCode == 200) {
-        print('주소가 성공적으로 전송되었습니다.');
-      } else {
-        print('주소 전송에 실패했습니다: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('주소 전송 중 에러 발생: $e');
-    }
-  }
+  final String orderUrl = '$locApi/orders/list?userId=';
 
   //내 주문목록 불러오기 => 백엔드 수정 필요
   Future<List<Order>> fetchOrders(int userId) async {
@@ -42,7 +20,7 @@ class OrderService {
     try {
       final response = await dio
           // .get('https://www.mystad.com/api/orders/list?userId=${userId}');
-          .get('http://10.0.2.2:8080/api/orders/list?userId=${userId}');
+          .get('${orderUrl}$userId');
       if (response.statusCode == 200) {
         List<dynamic> ordersJson = response.data['content'];
         List<Order> orders =
