3281f855b654b4269b65496f305e843cb6ca0fed
이태경 <korea9300@naver.com>
2024-04-29 16:05:42 +0900

    feat: getUserLocation 기능 추가

---
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/user/service/command/GetUserLocationCommand.java
@@ -0,0 +1,11 @@
+package com.klpc.stadspring.domain.user.service.command;
+
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+public class GetUserLocationCommand {
+    private Long userId;
+}
