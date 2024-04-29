831c4b9a9860d5eae6f2036e197aa3704b5f099b
최은희 <gilukji226@naver.com>
2024-04-29 13:49:18 +0900

    refactor: 주석 삭제

--- stad-spring/src/main/java/com/klpc/stadspring/domain/contents/categoryRelationship/service/ContentCategoryRelationshipService.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/categoryRelationship/service/ContentCategoryRelationshipService.java
@@ -20,8 +20,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ContentCategoryRelationshipService {
     private final ContentCategoryRelationshipRepository repository;
-//    private final ContentCategoryRepository categoryRepository;
-//    private final ContentConceptRepository conceptRepository;
 
     /**
      * 카테고리별 contentId 12개 조회
@@ -31,19 +29,4 @@ public class ContentCategoryRelationshipService {
                 .orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
         return list;
     }
-
-//    /**
-//     * 카테고리와 콘텐츠 개념 사이 관계 등록
-//     */
-//    @Transactional(readOnly = false)
-//
-//    public void addCategoryRelationship(AddCategoryRelationshipRequestCommand command) {
-//        ContentCategory category = categoryRepository.findById(command.getContentCategoryId())
-//                .orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
-//        ContentConcept concept = conceptRepository.findById(command.getContentConceptId())
-//                .orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
-//        ContentCategoryRelationship newRelationship = ContentCategoryRelationship.createRelationship(category, concept);
-//
-//        repository.save(newRelationship);
-//    }
 }
