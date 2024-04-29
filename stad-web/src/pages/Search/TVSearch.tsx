c4dfcca4d69ea12c8aa4ac48941ce77f309c61c9
이서윤 <s2yunn@gmail.com>
2024-04-30 07:52:00 +0900

    feat: 검색 페이지

--- stad-web/src/pages/Search/TVSearch.tsx
+++ stad-web/src/pages/Search/TVSearch.tsx
@@ -1,3 +1,66 @@
+import { ChangeEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import TVNav from "../../components/Nav/TVNav";
+import Content from "../../components/Container/Content";
+import { useMutation } from "react-query";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import { Video } from "../Category/TVDetail";
+import TVSearchInput from "./TVSearchInput";
+import styles from "./TVSearch.module.css";
+import TVContainer from "../../components/Container/TVContainer";
+
 export default function TVSearch() {
-  return <div>검색화면</div>;
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+  const [searchResults, setSearchResults] = useState<Video[] | null>(null);
+  // 검색 시도 상태 추가
+  const [searchAttempted, setSearchAttempted] = useState(false);
+  const accessToken = useSelector(
+    (state: RootState) => state.token.accessToken
+  );
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const nextValue = e.target.value;
+    console.log("검색 키워드", nextValue);
+    setSearchValue(nextValue);
+  }
+
+  return (
+    <div>
+      <TVContainer>
+        <TVNav />
+        <Content>
+          <TVSearchInput
+            value={searchValue}
+            onChange={handleChange}
+            onSearch={setSearchResults}
+            onSearchAttempted={() => setSearchAttempted(true)} // 검색 시도 됨 여부 체크
+          />
+          {searchValue.length > 0 && searchAttempted && (
+            <>
+              {searchResults && searchResults.length > 0 ? (
+                <>
+                  <div className={styles.gridContainer}>
+                    {searchResults.map((data, index) => (
+                      <div
+                        className={`${styles.thumbnail}`}
+                        key={index}
+                        onClick={() => navigate(`/tv/${data.id}`)}
+                      >
+                        <img src={data.thumbnailUrl} alt="영상 썸네일" />
+                        <div className={`${styles.title}`}>{data.title}</div>
+                      </div>
+                    ))}
+                  </div>
+                </>
+              ) : (
+                <div className={styles.noContent}>검색 결과가 없습니다.</div>
+              )}
+            </>
+          )}
+        </Content>
+      </TVContainer>
+    </div>
+  );
 }
