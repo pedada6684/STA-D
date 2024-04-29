7d9352a0561cad12c3aefbc664f865083a16502c
최은희 <gilukji226@naver.com>
2024-04-29 13:02:59 +0900

    feat: concept와 relationship이 한 transaction에서 생성되도록 수정

--- stad-spring/src/main/java/com/klpc/stadspring/domain/contents/concept/service/ContentConceptService.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/concept/service/ContentConceptService.java
@@ -1,23 +1,29 @@
 package com.klpc.stadspring.domain.contents.concept.service;
 
+import com.klpc.stadspring.domain.contents.category.entity.ContentCategory;
+import com.klpc.stadspring.domain.contents.category.repository.ContentCategoryRepository;
+import com.klpc.stadspring.domain.contents.categoryRelationship.entity.ContentCategoryRelationship;
+import com.klpc.stadspring.domain.contents.categoryRelationship.repository.ContentCategoryRelationshipRepository;
 import com.klpc.stadspring.domain.contents.concept.entity.ContentConcept;
 import com.klpc.stadspring.domain.contents.concept.repository.ContentConceptRepository;
 import com.klpc.stadspring.domain.contents.concept.service.command.request.AddConceptRequestCommand;
 import com.klpc.stadspring.global.response.ErrorCode;
 import com.klpc.stadspring.global.response.exception.CustomException;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Slf4j
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ContentConceptService {
     public final ContentConceptRepository repository;
+    public final ContentCategoryRelationshipRepository relationshipRepository;
+    public final ContentCategoryRepository categoryRepository;
 
     // id로 콘텐츠 개념 조회
     public ContentConcept getContentConceptById(Long id) {
@@ -34,6 +40,7 @@ public class ContentConceptService {
     }
 
     // contentConcept 등록
+    @Transactional(readOnly = false)
     public void addConcept(AddConceptRequestCommand command) {
         log.info("AddConceptRequestCommand : " + command);
 
@@ -48,5 +55,15 @@ public class ContentConceptService {
                 command.getThumbnail(),
                 command.getTitle());
         repository.save(newContentConcept);
+
+        for (int i = 0; i < command.getGenre().size(); i++) {
+            ContentCategory category = categoryRepository.findByIsMovieAndName(command.isMovie(),command.getGenre().get(i))
+                    .orElseThrow(() -> new CustomException(ErrorCode.ENTITIY_NOT_FOUND));
+
+            ContentCategoryRelationship newRelationship = ContentCategoryRelationship.createRelationship(
+                    category,
+                    newContentConcept);
+            relationshipRepository.save(newRelationship);
+        }
     }
 }
