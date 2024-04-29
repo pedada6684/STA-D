08ef37f3c26f68bce8f8a7812ee4608d2d2f8bcd
이서윤 <s2yunn@gmail.com>
2024-04-30 06:01:33 +0900

    feat: 기업 정보 조회, 수정 api 연결

--- stad-web/src/pages/MyPage/Enterprise/Profile.tsx
+++ stad-web/src/pages/MyPage/Enterprise/Profile.tsx
@@ -1,25 +1,44 @@
 import styles from "./Profile.module.css";
 import company from "../../../assets/profile_company.png";
 import { MouseEvent, useState } from "react";
+import { EnterpriseData } from "./EnterprisesEdit";
+import { profileImgUpload } from "./EnterpriseApi";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store";
 
 interface ProfileProps {
+  data?: EnterpriseData;
   onEditClick: (e: MouseEvent<HTMLDivElement>) => void;
+  onSaveClick: (e: MouseEvent<HTMLDivElement>) => void;
 }
 
-export default function Profile({ onEditClick }: ProfileProps) {
+export default function Profile({
+  data,
+  onEditClick,
+  onSaveClick,
+}: ProfileProps) {
   const [editing, setEditing] = useState(false); // 수정 or 저장 상태관리
   const [image, setImage] = useState(company); // 이미지 업로드 관리
-
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const userId = useSelector((state: RootState) => state.user.userId);
+  const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
-      let img = e.target.files[0];
-      setImage(URL.createObjectURL(img));
+      let profileImg = e.target.files[0]; // FileList에서 첫 번째 File 선택
+      const res = await profileImgUpload(profileImg, userId);
+      if (res) {
+        console.log("이미지 url", res);
+        setImage(res);
+      }
     }
   };
 
   const toggleEditing = (e: React.MouseEvent<HTMLDivElement>) => {
-    setEditing(!editing);
-    if (!editing) onEditClick(e); // 수정 모드 아닐 때는 onEditClick() 호출 x
+    if (editing) {
+      onSaveClick(e); // "저장" 버튼 클릭 시 이벤트
+      setEditing(false);
+    } else {
+      onEditClick(e); // "수정" 버튼 클릭 시 이벤트
+      setEditing(true);
+    }
   };
   return (
     <div className={`${styles.profile}`}>
@@ -44,7 +63,7 @@ export default function Profile({ onEditClick }: ProfileProps) {
             </label>
           )}
         </div>
-        <div className={`${styles.title}`}>회사명</div>
+        <div className={`${styles.title}`}>{data?.company}</div>
       </div>
       <div className={`${styles.edit}`} onClick={toggleEditing}>
         {editing ? "저장" : "수정"}
