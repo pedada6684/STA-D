4d6fd286bb4ca8af34582a0c4f0f3ea4e2c31a4d
imin <lllgee@naver.com>
2024-05-02 21:16:39 +0900

    feat : 상품 별 주문량 cnt 기능 추가

--- stad-spring/src/main/java/com/klpc/stadspring/domain/orders/repository/OrdersRepository.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/orders/repository/OrdersRepository.java
@@ -3,11 +3,19 @@ package com.klpc.stadspring.domain.orders.repository;
 import com.klpc.stadspring.domain.orders.entity.Orders;
 import com.klpc.stadspring.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrdersRepository extends JpaRepository<Orders,Long> {
 
     public List<Orders> findAllByUser(User user);
+
+    @Query("""
+              SELECT COUNT(od) FROM Orders od
+              WHERE :productTypeId in (SELECT pt.productType.id FROM od.orderProducts pt)
+           """)
+    public Optional<Long> findCntOrdersByProductType(Long productTypeId);
 
 }
