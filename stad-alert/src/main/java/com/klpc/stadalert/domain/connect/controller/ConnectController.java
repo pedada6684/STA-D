db9c8cccaea0df4f66798ea0f7537313d5efdeec
이태경 <korea9300@naver.com>
2024-05-02 15:59:33 +0900

    feat: alert 서버 Swagger 연결

--- stad-alert/src/main/java/com/klpc/stadalert/domain/connect/controller/ConnectController.java
+++ stad-alert/src/main/java/com/klpc/stadalert/domain/connect/controller/ConnectController.java
@@ -7,28 +7,26 @@ import com.klpc.stadalert.global.service.SseEmitters;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/connect")
 public class ConnectController {
 
 	final SseEmitters sseEmitters;
 	final ConnectService connectService;
 
-	@PostMapping("/connect")
+	@PostMapping()
 	public ResponseEntity<SseEmitter> connect(@RequestBody ConnectRequest request) {
 		log.info("CreateConnectRequest: "+ request);
 		SseEmitter emitter = sseEmitters.subscribe(request.toCommand());
 		return ResponseEntity.ok(emitter);
 	}
 
-	@PostMapping("qrlogin")
+	@PostMapping("/qrlogin")
 	public ResponseEntity<?> qrLogin(@RequestBody QrLoginRequest request) {
 		log.info("CreateConnectRequest: "+ request);
 		connectService.qrLogin(request.toCommand());
