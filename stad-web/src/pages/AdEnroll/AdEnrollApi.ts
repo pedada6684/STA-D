4acee257688dcd6e2f17868321007f04e841d9a4
imin <lllgee@naver.com>
2024-05-02 23:02:37 +0900

    feat : advert delete 수정

--- stad-web/src/pages/AdEnroll/AdEnrollApi.ts
+++ stad-web/src/pages/AdEnroll/AdEnrollApi.ts
@@ -91,3 +91,21 @@ export const getAdvertList = async (userId : number) => {
     }
 };
 
+export const deleteAdvert = async (advertId : number | undefined) => {
+    try {
+        const response = await fetch(`/api/advert?advertId=${advertId}`, {
+            method: 'DELETE',
+        });
+        if (!response.ok) {
+            throw new Error('광고 삭제 실패');
+        }
+        const data = await response.json(); // JSON 형태로 응답 받음
+        const result = data.result;
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.error('광고 삭제 실패 : ', error);
+        return null;
+    }
+};
+
