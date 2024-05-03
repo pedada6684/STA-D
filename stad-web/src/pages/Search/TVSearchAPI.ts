4df2514cab669ae78eb2e1a262a40711f1d2c89f
이서윤 <s2yunn@gmail.com>
2024-05-03 11:10:04 +0900

    fix: 오류 수정
    Former-commit-id: 90c06792dacabb217101274888f560cae17fbaaf

--- stad-web/src/pages/Search/TVSearchAPI.ts
+++ stad-web/src/pages/Search/TVSearchAPI.ts
@@ -3,13 +3,7 @@ import axios from "axios";
 export async function GetSearch(value: string, accessToken: string | null) {
   try {
     const response = await axios.get(
-      `/api/contents-concept/search/${encodeURIComponent(value)}`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-        },
-      }
+      `/api/contents-concept/search/${encodeURIComponent(value)}`
     );
     console.log(response.data);
     return response.data;
