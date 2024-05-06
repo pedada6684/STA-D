8972222eb8e8914421096a6e76b7382dff247756
이서윤 <s2yunn@gmail.com>
2024-05-07 04:49:51 +0900

    feat: 각 페이지 컴포넌트
    Former-commit-id: 1f5bf66c56b17d1ea1dcb72ac0af04f0d2a62c9b

--- stad-web/src/components/AdManagement/GetClick.tsx
+++ stad-web/src/components/AdManagement/GetClick.tsx
@@ -1,3 +1,100 @@
-export default function GetClick() {
-  return <div></div>;
+import { useEffect, useState } from "react";
+import { AdvertIdProps } from "../../pages/AdManagement/All/AllExposure";
+import { RootState } from "../../store";
+import { useSelector } from "react-redux";
+import { getClickCount } from "./DashboardAPI";
+import { ApexOptions } from "apexcharts";
+import styles from "./ChartData.module.css";
+import ReactApexChart from "react-apexcharts";
+
+export default function GetClick({ advertId }: AdvertIdProps) {
+  const accessToken = useSelector(
+    (state: RootState) => state.token.accessToken
+  );
+  const [seriesData, setSeriesData] = useState([
+    { name: "광고클릭수", data: [] }, // 초기 데이터 빈 배열로 설정
+  ]);
+
+  useEffect(() => {
+    if (advertId && accessToken) {
+      getClickCount(advertId, accessToken).then((apiData) => {
+        if (apiData && apiData.list) {
+          const chartData = apiData.list.map(
+            (item: { date: string; value: number }) => ({
+              x: item.date,
+              y: item.value,
+            })
+          );
+          setSeriesData([{ name: "광고클릭수", data: chartData }]);
+        }
+      });
+    }
+  }, [advertId, accessToken]);
+  const [chartOptions, setChartOptions] = useState<ApexOptions>({
+    chart: {
+      type: "area",
+      height: 350,
+      zoom: {
+        enabled: false,
+      },
+      stacked: true,
+      dropShadow: {
+        enabled: true,
+        top: -2,
+        left: 2,
+        blur: 5,
+        opacity: 0.06,
+      },
+    },
+    colors: ["#00127A"], // 그래프 색상 설정
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "straight",
+    },
+    title: {
+      text: "광고 클릭 수",
+      align: "left",
+      style: {
+        fontFamily: "NotoSans KR",
+        fontSize: "1.5rem",
+        fontWeight: "800",
+      },
+      offsetY: 20, // 제목 위치 조정
+    },
+    subtitle: {
+      text: "30일 동안의 광고 클릭 수를 보여줍니다.",
+      align: "left",
+      style: {
+        fontSize: "1.1rem",
+        fontWeight: "500",
+      },
+      offsetY: 50, // 제목 위치 조정
+    },
+    // labels: dates,
+    xaxis: {
+      type: "datetime",
+    },
+    yaxis: {
+      opposite: true,
+    },
+    legend: {
+      horizontalAlign: "left",
+    },
+  });
+
+  return (
+    <div className={`${styles.mainChart}`}>
+      <div id="chart">
+        <ReactApexChart
+          options={chartOptions}
+          series={seriesData}
+          type="area"
+          height={300}
+        />
+      </div>
+      <div id="html-dist"></div>
+    </div>
+  );
 }
--- stad-web/src/components/AdManagement/GetOrder.tsx
+++ stad-web/src/components/AdManagement/GetOrder.tsx
@@ -1,3 +1,100 @@
-export default function GetOrder() {
-  return <div></div>;
+import { useSelector } from "react-redux";
+import { AdvertIdProps } from "../../pages/AdManagement/All/AllExposure";
+import { RootState } from "../../store";
+import { useEffect, useState } from "react";
+import { getOrderCount } from "./DashboardAPI";
+import { ApexOptions } from "apexcharts";
+import styles from "./FrameComponent.module.css";
+import ReactApexChart from "react-apexcharts";
+
+export default function GetOrder({ advertId }: AdvertIdProps) {
+  const accessToken = useSelector(
+    (state: RootState) => state.token.accessToken
+  );
+  const [seriesData, setSeriesData] = useState([
+    { name: "주문건수", data: [] }, // 초기 데이터 빈 배열로 설정
+  ]);
+
+  useEffect(() => {
+    if (advertId && accessToken) {
+      getOrderCount(advertId, accessToken).then((apiData) => {
+        if (apiData && apiData.list) {
+          const chartData = apiData.list.map(
+            (item: { date: string; value: number }) => ({
+              x: item.date,
+              y: item.value,
+            })
+          );
+          setSeriesData([{ name: "주문건수", data: chartData }]);
+        }
+      });
+    }
+  }, [advertId, accessToken]);
+  const [chartOptions, setChartOptions] = useState<ApexOptions>({
+    chart: {
+      type: "area",
+      height: 350,
+      zoom: {
+        enabled: false,
+      },
+      stacked: true,
+      dropShadow: {
+        enabled: true,
+        top: -2,
+        left: 2,
+        blur: 5,
+        opacity: 0.06,
+      },
+    },
+    colors: ["#00127A"], // 그래프 색상 설정
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "straight",
+    },
+    title: {
+      text: "주문건수",
+      align: "left",
+      style: {
+        fontFamily: "NotoSans KR",
+        fontSize: "1.5rem",
+        fontWeight: "800",
+      },
+      offsetY: 20, // 제목 위치 조정
+    },
+    subtitle: {
+      text: "30일 동안의 주문건수를 보여줍니다.",
+      align: "left",
+      style: {
+        fontSize: "1.1rem",
+        fontWeight: "500",
+      },
+      offsetY: 50, // 제목 위치 조정
+    },
+    // labels: dates,
+    xaxis: {
+      type: "datetime",
+    },
+    yaxis: {
+      opposite: true,
+    },
+    legend: {
+      horizontalAlign: "left",
+    },
+  });
+
+  return (
+    <div className={`${styles.mainChart}`}>
+      <div id="chart">
+        <ReactApexChart
+          options={chartOptions}
+          series={seriesData}
+          type="area"
+          height={300}
+        />
+      </div>
+      <div id="html-dist"></div>
+    </div>
+  );
 }
