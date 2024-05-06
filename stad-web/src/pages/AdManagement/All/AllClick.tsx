5b5bf0524b1ea9ae0da8ab5481ce4cfdf5639854
이서윤 <s2yunn@gmail.com>
2024-05-07 04:56:20 +0900

    feat: 각 컴포넌트마다 api 연결
    Former-commit-id: 3e0d57b749b1de58444f01451b685dfafae53acf

--- stad-web/src/pages/AdManagement/All/AllClick.tsx
+++ stad-web/src/pages/AdManagement/All/AllClick.tsx
@@ -1,3 +1,24 @@
-export default function AllClick() {
-  return <div></div>;
+import GetClick from "../../../components/AdManagement/GetClick";
+import { AdvertIdProps } from "./AllExposure";
+import styles from "../AdManagement.module.css";
+import PieChart from "../../../components/AdManagement/PieChart";
+import BarChart from "../../../components/AdManagement/BarChart";
+import Frame from "../../../components/AdManagement/Frame";
+export default function AllClick({ advertId }: AdvertIdProps) {
+  return (
+    <div className={`${styles.etcContainer}`}>
+      <GetClick advertId={advertId} />
+      <Frame advertId={advertId} />
+      <div className={`${styles.chartWrapper}`}>
+        <PieChart
+          title="30일 동안 광고 클릭 비율"
+          dataType="totalAdvertClick"
+        />
+        <BarChart
+          title="30일 동안 광고 클릭 건수"
+          dataType="totalAdvertClick"
+        />
+      </div>
+    </div>
+  );
 }
