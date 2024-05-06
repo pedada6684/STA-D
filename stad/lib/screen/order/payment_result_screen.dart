d96da4c4ac9a20428837be5726bb22a02cd69be1
김은지 <eungg0214@naver.com>
2024-05-06 21:03:22 +0900

    feat: 결제하기 구현

---
+++ stad/lib/screen/order/payment_result_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaymentResultScreen extends StatefulWidget {
+  const PaymentResultScreen({super.key});
+
+  @override
+  State<PaymentResultScreen> createState() => _PaymentResultScreenState();
+}
+
+class _PaymentResultScreenState extends State<PaymentResultScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
