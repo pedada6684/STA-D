e6f9e9bf34b1dca4875bdd48bd436f47231a2132
이서윤 <s2yunn@gmail.com>
2024-05-07 03:47:10 +0900

    feat: frame 컴포넌트
    Former-commit-id: 5e51adae0918536a14c6e720b457ebadfb12de35

---
+++ stad-web/src/components/AdManagement/FrameComponent.tsx
@@ -0,0 +1,48 @@
+import styles from "./FrameComponent.module.css";
+interface FrameProps {
+  icon: string;
+  title: string;
+  value?: number;
+  className?: string;
+  iconContainer?: string;
+  unit: string;
+}
+export default function FrameComponent({
+  icon,
+  title,
+  value,
+  className,
+  iconContainer,
+  unit,
+}: FrameProps) {
+  const stylesContainer = className
+    ? `${styles.container} ${styles[className] || ""}`
+    : styles.container;
+  const iconWrapper = iconContainer
+    ? `${styles.iconWrapper} ${styles[iconContainer]}`
+    : styles.iconWrapper;
+  // unit 값에 따라 다르게 렌더링
+  const valueDisplay =
+    unit === "건" ? (
+      <>
+        {value}
+        <span>{unit}</span>
+      </>
+    ) : (
+      <>
+        <span>{unit}</span>
+        {value}
+      </>
+    );
+  return (
+    <div className={stylesContainer}>
+      <div className={iconWrapper}>
+        <img src={icon} alt="아이콘" className={`${styles.icon}`} />
+      </div>
+      <div className={`${styles.textWrapper}`}>
+        <div className={`${styles.title}`}>{title}</div>
+        <div className={`${styles.value}`}>{valueDisplay}</div>
+      </div>
+    </div>
+  );
+}
