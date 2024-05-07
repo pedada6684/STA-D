9c05b67e7d863027ee47906d6faa3f14ba27c63b
이서윤 <s2yunn@gmail.com>
2024-05-07 10:45:39 +0900

    design: 스크롤 페이지 마다 Dots 적용

--- stad-web/src/pages/AdManagement/AdManagement.tsx
+++ stad-web/src/pages/AdManagement/AdManagement.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import WebNav from "../../components/Nav/WebNav";
 import SelectAdListBox from "../../components/Select/SelectAdListBox";
 import styles from "./AdManagement.module.css";
@@ -7,12 +7,39 @@ import AllClick from "./All/AllClick";
 import SecondContent from "../../components/Container/SecondContent";
 import AllOrder from "./All/AllOrder";
 import AllRevenue from "./All/AllRevenue";
+import Dots from "./Dots/Dots";
 export default function AdManagement() {
   // selectAdList에서 갖고온 value를 props로 내보내기 위한 useState
   const [advertId, setAdvertId] = useState<number | null>(null);
+  const [scrollIdx, setScrollIdx] = useState(1);
+  const mainWrapperRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const wheelHandler = (e: WheelEvent) => {
+      e.preventDefault();
+      const { deltaY } = e;
+      const pageHeight = window.innerHeight;
+      const newScrollIdx = scrollIdx + (deltaY > 0 ? 1 : -1);
+      if (newScrollIdx >= 1 && newScrollIdx <= 4) {
+        // 페이지 범위 제한
+        mainWrapperRef.current?.scrollTo({
+          top: pageHeight * (newScrollIdx - 1),
+          left: 0,
+          behavior: "smooth",
+        });
+        setScrollIdx(newScrollIdx);
+      }
+    };
+
+    const refCurrent = mainWrapperRef.current;
+    refCurrent?.addEventListener("wheel", wheelHandler);
+    return () => {
+      refCurrent?.removeEventListener("wheel", wheelHandler);
+    };
+  }, [scrollIdx]);
   return (
-    <div className={`${styles.scrollSnapContainer}`}>
+    <div className={`${styles.scrollSnapContainer}`} ref={mainWrapperRef}>
       <WebNav />
+      <Dots scrollIdx={scrollIdx} />
       <SecondContent>
         <div className={`${styles.firstWrapper}`}>
           <div className={`${styles.main}`}>
