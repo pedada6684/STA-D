e6e09317eb01d49d0b6ff2441797b4070c4b8cdb
최은희 <gilukji226@naver.com>
2024-05-08 00:12:34 +0900

    feat : 최근 시청한 영상 20개로 유저 관심사 3개 추출

--- stad-spring/src/main/java/com/klpc/stadspring/domain/contents/label/repository/ContentLabelRepository.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/label/repository/ContentLabelRepository.java
@@ -2,6 +2,9 @@ package com.klpc.stadspring.domain.contents.label.repository;
 
 import com.klpc.stadspring.domain.contents.label.entity.ContentLabel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ContentLabelRepository extends JpaRepository<ContentLabel, Long> {
+    @Query("SELECT cl.name FROM ContentLabel cl WHERE cl.id = :id")
+    String findNameById(Long id);
 }
