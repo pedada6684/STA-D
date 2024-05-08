642341d37768fdff84637ade59304d1ff1f0b677
이서윤 <s2yunn@gmail.com>
2024-05-08 15:31:23 +0900

    feat: qr로그인 1차 구현
    Former-commit-id: 464292f86c1478693c5ca50097ce8cb0e906af99

--- stad-web/src/pages/TVLogin/TestServer.tsx
+++ stad-web/src/pages/TVLogin/TestServer.tsx
@@ -1,22 +1,43 @@
 // 클라이언트 측에서 특정할 만한 sessionID를 생성하여 저장 (이때는 게스트라고 생각)
-// qr 생성 시 내 정보를 담고서 qr 생성
-// qr 인식 후 유저 식별 후 데이터 저장 ?
-// 로그인 한 후엔 유령계정 아니라고 또 서버에 요청보내야함
+// 로그인 페이지에 qr 생성 시 저장한 세션 아이디를 value로 담아주기
+// qr 인식 후 앱에서는 sse로 로그인 요청으로 서버에 api 요청
+// 로그인 후 서버에 서버에 CONNECT 요청 보내기
 
 // eventSource 객체가 생성된다면,  네트워크 탭에서 event 스트림 관찰 가능
-//
 import { NativeEventSource, EventSourcePolyfill } from "event-source-polyfill";
-import { useDispatch } from "react-redux";
+export interface postProps {
+  userId?: number;
+  sessionId?: string;
+  type?: string;
+}
 export default function TestServer() {
-  const dispatch = useDispatch();
   const EventSource = NativeEventSource || EventSourcePolyfill;
-  const URL = "http://localhost:8080"; // 로컬로 테스트 진행하고 성공 시 서버 URL로 바꿔서 올리기
-  const sessionId = ``;
+  const URL = "http://localhost:8081"; // 로컬로 테스트 진행하고 성공 시 서버 URL로 바꿔서 올리기
+  // post 요청으로 사용자 정보 보내기 (연결을 위한 api post요청)
+  // const sendLoginRequest = async ({ userId, sessionId, type }: postProps) => {
+  //   try {
+  //     const response = await fetch(`${URL}/alert/connect`, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json", // 요청 본문이 JSON임을 명시
+  //       },
+  //       body: JSON.stringify({
+  //         userId: userId,
+  //         tmpId: sessionId,
+  //         type: type,
+  //       }),
+  //     });
+  //     const data = await response.json(); // 응답을 JSON으로 변환
+  //     console.log("서버 응답:", data);
+  //   } catch (error) {
+  //     console.error("연결 에러:", error);
+  //   }
+  // };
+
+  // 연결 부분
   const handleConnect = () => {
-    const eventSource = new EventSource(
-      // api 연결 부분 (header에 jwt 토큰 요청시 event-source-polyfill)
-      URL + `/api/`
-    );
+    // api 연결 부분 (header에 jwt 토큰 요청시 event-source-polyfill 필요)
+    const eventSource = new EventSource(`${URL}/alert/connect/`);
 
     // 연결 성공
     eventSource.onopen = (event) => {
@@ -26,6 +47,7 @@ export default function TestServer() {
     // 연결 실패
     eventSource.onerror = (e) => {
       console.log("연결 실패", e);
+      eventSource.close(); // 연결 끊기
     };
 
     // 이벤트 리스너
