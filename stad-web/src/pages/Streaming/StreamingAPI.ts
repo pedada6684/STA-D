dbe9ae2e143f0ae9a38066facfab2335de00e75d
최은희 <gilukji226@naver.com>
2024-05-08 16:28:50 +0900

    feat : 광고 url 리스트 조회
    Former-commit-id: 28fb0b9b396eb8ffd4defe70c43b7ec1319040ae

--- stad-web/src/pages/Streaming/StreamingAPI.ts
+++ stad-web/src/pages/Streaming/StreamingAPI.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-export async function getVideoConcept(
-  accessToken: string | null,
-  detailId: number
-) {
+export async function getVideoConcept(accessToken: string | null, detailId: number) {
   try {
     const response = await axios.get(`/api/contents-detail/${detailId}`, {});
     console.log("영상 상세 정보 조회 성공", response.data);
@@ -13,14 +10,9 @@ export async function getVideoConcept(
   }
 }
 
-export async function getSeriesVideoList(
-  accessToken: string | null,
-  category: string
-) {
+export async function getSeriesVideoList(accessToken: string | null, category: string) {
   try {
-    const response = await axios.get(
-      `/api/contents-concept/series/${category}`
-    );
+    const response = await axios.get(`/api/contents-concept/series/${category}`);
     console.log("(시리즈)카테고리 영상 목록 조회 성공", response.data);
     return response.data.responseList;
   } catch (error) {
@@ -28,10 +20,7 @@ export async function getSeriesVideoList(
   }
 }
 
-export async function getMovieVideoList(
-  accessToken: string | null,
-  category: string
-) {
+export async function getMovieVideoList(accessToken: string | null, category: string) {
   try {
     const response = await axios.get(`/api/contents-concept/movie/${category}`);
     console.log("(영화)카테고리 영상 목록 조회 성공", response.data);
@@ -41,14 +30,9 @@ export async function getMovieVideoList(
   }
 }
 
-export async function getStreaming(
-  accessToken: string | null,
-  detailId: number
-) {
+export async function getStreaming(accessToken: string | null, detailId: number) {
   try {
-    const response = await axios.get(
-      `/api/contents-detail/streaming/${detailId}`
-    );
+    const response = await axios.get(`/api/contents-detail/streaming/${detailId}`);
     console.log("스트리밍 성공", response.data);
     return response.data;
   } catch (error) {
@@ -56,10 +40,7 @@ export async function getStreaming(
   }
 }
 
-export async function getVideoDetail(
-  accessToken: string | null,
-  conceptId: number
-) {
+export async function getVideoDetail(accessToken: string | null, conceptId: number) {
   // conceptId로 detail 조회
   try {
     console.log(conceptId);
@@ -75,11 +56,7 @@ export async function getVideoDetail(
   }
 }
 // 시청 영상 등록
-export async function postWatchAdd(
-  accessToken: string | null,
-  userId: number,
-  detailId: number
-) {
+export async function postWatchAdd(accessToken: string | null, userId: number, detailId: number) {
   try {
     console.log("detailId", detailId, "/userId", userId);
     const response = await axios.post(`/api/contents-watch/add`, {
@@ -90,5 +67,20 @@ export async function postWatchAdd(
     return response.data;
   } catch (error) {
     console.error("시청 영상 생성 실패", error);
+  }
+}
+// 광고 url 리스트 조회
+export async function getAdvertUrlList(accessToken: string | null, userId: number, detailId: number) {
+  try {
+    console.log("detailId", detailId, "/userId", userId);
+    const response = await axios.get(`/api/advert-video/get-video-list/${detailId}`, {
+      params: {
+        userId: userId,
+      },
+    });
+    console.log("광고 url 리스트 조회 성공", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("광고 url 리스트 조회 실패", error);
   }
 }
