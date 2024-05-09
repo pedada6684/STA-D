43b5ff251852b8453537f83c31edd8e02c5e55ef
이태경 <korea9300@naver.com>
2024-05-10 02:01:19 +0900

    feat: RedisCache 세팅

--- stad-spring/src/main/java/com/klpc/stadspring/StadSpringApplication.java
+++ stad-spring/src/main/java/com/klpc/stadspring/StadSpringApplication.java
@@ -5,11 +5,13 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableBatchProcessing // 배치 기능 활성화
 @EnableScheduling
+@EnableCaching
 public class StadSpringApplication {
 
 	public static void main(String[] args) {
