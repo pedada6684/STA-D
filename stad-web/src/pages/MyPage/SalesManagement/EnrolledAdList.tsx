34f864b89a89e521ce3a09b89a43639dc9ad2026
이서윤 <s2yunn@gmail.com>
2024-05-09 09:31:00 +0900

    feat: 광고/상품 라우터 추가
    Former-commit-id: c6ada9e82209c6aa270a506927c7a62e2f1c32ca

--- stad-web/src/pages/MyPage/SalesManagement/EnrolledAdList.tsx
+++ stad-web/src/pages/MyPage/SalesManagement/EnrolledAdList.tsx
@@ -1,48 +1,55 @@
 import { useEffect, useState } from "react";
 import styles from "./EnrolledList.module.css";
 import dummyData from "./dummyAdData.json";
-import {deleteAdvert, getAdvertList, getContentConcept, modifyAdvert} from "../../AdEnroll/AdEnrollApi";
-import {useSelector} from "react-redux";
-import {RootState} from "../../../store";
+import {
+  deleteAdvert,
+  getAdvertList,
+  getContentConcept,
+  modifyAdvert,
+} from "../../AdEnroll/AdEnrollApi";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store";
+import { useNavigate } from "react-router";
 
 interface adType {
-  advertId? : number;
-  title? : string;
-  description? : string;
-  startDate? : string;
-  endDate? : string;
-  advertType? : string;
-  advertCategory? : string;
+  advertId?: number;
+  title?: string;
+  description?: string;
+  startDate?: string;
+  endDate?: string;
+  advertType?: string;
+  advertCategory?: string;
   directVideoUrl?: string;
   bannerImgUrl?: string;
-  selectedContentList? : number[];
-  advertVideoUrlList? : string[];
+  selectedContentList?: number[];
+  advertVideoUrlList?: string[];
 }
 
 export default function EnrolledAdList() {
   const [adsList, setAdsList] = useState<adType[]>([]); // 광고 목록
-  const userId = useSelector((state: RootState)=> state.user.userId);
-
+  const userId = useSelector((state: RootState) => state.user.userId);
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchAdsList = async () => {
       try {
         const data = await getAdvertList(userId);
         setAdsList(data);
       } catch (error) {
-        console.error('광고 목록 조회 실패 : ', error);
+        console.error("광고 목록 조회 실패 : ", error);
       }
     };
 
     fetchAdsList();
   }, [adsList]);
 
-  const editClick = (advert : adType) => {
-    console.log(`Edit clicked for ad with ID:`);
-    modifyAdvert(advert)
+  const editClick = (advert: adType) => {
+    console.log(`Edit clicked for ad with ID:`, advert.advertId);
+    navigate(`/ads/${advert.advertId}`);
+    // modifyAdvert(advert);
   };
 
-  const deleteClick = (advertId : number | undefined) => {
-    console.log(`Delete clicked for ad with ID:`+advertId);
+  const deleteClick = (advertId: number | undefined) => {
+    console.log(`Delete clicked for ad with ID:` + advertId);
     deleteAdvert(advertId);
     // window.location.reload();
   };
@@ -56,23 +63,31 @@ export default function EnrolledAdList() {
         <div className={`${styles.gridCategory}`}>광고기간</div>
         <div className={`${styles.gridCategory} `}>수정/삭제</div>
         {adsList.map((ad) => (
-            <>
+          <>
             <div className={`${styles.gridItem} ${styles.gridImg}`}>
-              <img src={ad.bannerImgUrl} alt="Ad"/>
+              <img src={ad.bannerImgUrl} alt="Ad" />
             </div>
             <div className={`${styles.gridItem}`}>{ad.title}</div>
             <div className={`${styles.gridItem}`}>
               {ad.startDate} ~ {ad.endDate}
             </div>
             <div className={`${styles.gridItem}`}>
-              <span style={{cursor: 'pointer', marginRight: '10px'}}
-                    onClick={() => editClick(ad)}>수정</span>
+              <span
+                style={{ cursor: "pointer", marginRight: "10px" }}
+                onClick={() => editClick(ad)}
+              >
+                수정
+              </span>
               <span>/</span>
-              <span style={{cursor: 'pointer', marginLeft: '10px'}}
-                    onClick={() => deleteClick(ad.advertId)}>삭제</span>
+              <span
+                style={{ cursor: "pointer", marginLeft: "10px" }}
+                onClick={() => deleteClick(ad.advertId)}
+              >
+                삭제
+              </span>
             </div>
           </>
-          ))}
+        ))}
       </div>
     </div>
   );
