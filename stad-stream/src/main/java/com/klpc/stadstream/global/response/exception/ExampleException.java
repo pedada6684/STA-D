cae5e1b9da18af16632d1893f727350be371ed17
이태경 <korea9300@naver.com>
2024-05-10 03:15:25 +0900

    feat: stad-stream 서버 추가

---
+++ stad-stream/src/main/java/com/klpc/stadstream/global/response/exception/ExampleException.java
@@ -0,0 +1,7 @@
+package com.klpc.stadstream.global.response.exception;
+
+public class ExampleException extends BusinessException {
+    public ExampleException(String message) {
+        super(message);
+    }
+}
