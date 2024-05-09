7c3e7ba7490f79ebecc5603078879fe55e3dbbde
최은희 <gilukji226@naver.com>
2024-05-09 23:34:27 +0900

    feat : 클릭 한번으로 광고 리스트부터 콘텐츠까지 재생

--- stad-spring/src/main/java/com/klpc/stadspring/domain/contents/detail/service/command/response/GetDetailListByConceptIdResponseCommand.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/detail/service/command/response/GetDetailListByConceptIdResponseCommand.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class GetDetailListByConceptIdResponseCommand {
+    private Long detailId;
     private Integer episode;
     private String videoUrl;
     private String summary;
