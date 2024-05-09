ff62c0d0e69b756963b1381cf613e85ff1100376
이서윤 <s2yunn@gmail.com>
2024-05-09 17:27:11 +0900

    feat: TV qr-login 구현

--- stad-web/src/pages/TVLogin/TVLogin.tsx
+++ stad-web/src/pages/TVLogin/TVLogin.tsx
@@ -4,12 +4,23 @@ import logo from "../../assets/tv_STA_D.png";
 import { useEffect, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import qrcode from "qrcode-generator";
-import { postProps } from "./TestServer";
-import axios from "axios";
+import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+import { tvUserActions } from "../../store/tvUser";
+interface appUserType {
+  userId: number;
+  nickname: string;
+  profile: string;
+}
 export default function TVLogin() {
   const [sessionId, setSessionId] = useState("");
   const qrContainerRef = useRef<HTMLDivElement>(null); // QR코드 렌더링할 컨테이너 참조
   const URL = "http://localhost:8081"; // 로컬로 테스트 진행하고 성공 시 서버 URL로 바꿔서 올리기
+  // const EventSource = NativeEventSource || EventSourcePolyfill;
+  const [userProfile, setUserProfile] = useState<appUserType>();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   useEffect(() => {
     // 세션 ID 생성
     const newSessionId = uuidv4();
@@ -25,18 +36,54 @@ export default function TVLogin() {
       qr.make();
       qrContainerRef.current.innerHTML = qr.createSvgTag({}); // SVG 태그 생성하여 할당
     }
+
+    // api 연결 부분 (header에 jwt 토큰 요청시 event-source-polyfill 필요)
+    const eventSource = new EventSource(
+      `${URL}/alert/connect/${"tv"}/${sessionId}/${0}`
+    );
+
+    // 연결 성공
+    eventSource.onopen = (event) => {
+      console.log("연결 성공", event);
+    };
+
+    // 이벤트 리스너
+    // connect 연결 시도 부분
+    eventSource.addEventListener("connect", (e) => {
+      const { data: receivedData } = e;
+      console.log("connect", receivedData);
+    });
+    // qr 로그인으로 App유저 프로필 받아오기
+    eventSource.addEventListener("Qr login", (e) => {
+      const { data: receivedData } = e;
+      const data = JSON.parse(e.data);
+      console.log(data);
+      setUserProfile(data);
+      console.log(userProfile);
+      console.log("QRLogin", receivedData);
+    });
+
+    // 연결 실패
+    eventSource.onerror = (e) => {
+      console.log("연결 실패", e);
+      eventSource.close(); // 연결 끊기
+    };
   }, [sessionId]); // sessionId 변경될 때마다 QR 코드 갱신
 
-  const sendRequest = async ({ sessionId }: postProps) => {
-    try {
-      const response = await axios.post(`${URL}/alert/connect`, {
-        tmpId: sessionId,
-      });
-      console.log("서버 응답:", response);
-    } catch (error) {
-      console.error("전달 실패", error);
+  useEffect(() => {
+    console.log("Updated userProfile:", userProfile);
+    if (userProfile) {
+      dispatch(
+        tvUserActions.loginUser({
+          userId: userProfile.userId,
+          userNickname: userProfile.nickname,
+          profile: userProfile.profile,
+        })
+      );
+      navigate("/tv-profile");
     }
-  };
+  }, [userProfile]); // userProfile 상태가 변경될 때마다 실행
+
   return (
     <div>
       <TVContainer>
