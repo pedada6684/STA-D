cdfcf2a3e04635fd8798f811cac153d805663c58
이태경 <korea9300@naver.com>
2024-05-10 03:15:25 +0900

    feat: stad-stream 서버 추가

---
+++ stad-stream/src/main/java/com/klpc/stadstream/domain/detail/repository/ContentDetailRepository.java
@@ -0,0 +1,8 @@
+package com.klpc.stadstream.domain.detail.repository;
+
+
+import com.klpc.stadstream.domain.detail.entity.ContentDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContentDetailRepository extends JpaRepository<ContentDetail, Long> {
+}
