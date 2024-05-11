97b6c15360c56349a7cc881ae55cb63bb3df7684
이서윤 <s2yunn@gmail.com>
2024-05-12 06:38:23 +0900

    fix: 토글 삭제
    Former-commit-id: 9d56b84076efcb54a3e87e1fc062c22b893b57a1

--- stad-web/src/components/Enroll/Digital.tsx
+++ stad-web/src/components/Enroll/Digital.tsx
@@ -1,9 +1,9 @@
-import {ChangeEvent, useEffect, useState} from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import styles from "./Advertisement.module.css";
 import ToggleButton from "../Button/ToggleButton";
 import InputContainer from "../Container/InputContainer";
 import EnrollButton from "../Button/EnrollButton";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 interface digitalForm {
   userId?: number;
   title?: string;
@@ -24,7 +24,7 @@ interface goodsForm {
   thumbnail?: String;
   cityDeliveryFee?: number;
   mtDeliveryFee?: number;
-  expStart?: string
+  expStart?: string;
   expEnd?: string;
   productTypeList?: ProductType[];
 }
@@ -45,7 +45,6 @@ interface ProductOption {
   optionValue: number; // 옵션값
 }
 export default function Digital() {
-
   const location = useLocation();
   const initialFormData: digitalForm = location.state;
   const [formData, setFormData] = useState<digitalForm>(initialFormData);
@@ -79,12 +78,9 @@ export default function Digital() {
           <div className={`${styles.name}`}>
             메인링크<span>*</span>
           </div>
-          <ToggleButton
-            isExpanded={isDigitalMainExpanded}
-            onToggle={toggleDigitalMain}
-          />
+          <ToggleButton />
         </div>
-        {isDigitalMainExpanded && (
+        {!isDigitalMainExpanded && (
           <InputContainer>
             <div className={`${styles.linkUrl}`}>
               <div className={`${styles.subTitle}`}>연결 URL</div>
@@ -101,7 +97,11 @@ export default function Digital() {
           </InputContainer>
         )}
       </div>
-      <EnrollButton goodsFormData={goodsFormData} formData={formData} from="digital"/>
+      <EnrollButton
+        goodsFormData={goodsFormData}
+        formData={formData}
+        from="digital"
+      />
     </div>
   );
 }
