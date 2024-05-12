3e70049c0f7235d86caaeb252387dd2afb43fb33
imin <lllgee@naver.com>
2024-05-12 15:50:53 +0900

    feat : product 조회 수정
    feat : product 조회 수정

--- stad-spring/src/main/java/com/klpc/stadspring/domain/product/controller/response/GetProductInfoResponse.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/product/controller/response/GetProductInfoResponse.java
@@ -20,27 +20,16 @@ public class GetProductInfoResponse {
      *  상품 정보 조회
      */
 
-    private Long id;
+    private Long productId;
     private String name;
-    private List<ProductImage> images;
+    private List<String> images;
     private String thumbnail;
     private Long cityDeliveryFee;
     private Long mtDeliveryFee;
     private LocalDateTime expStart;
     private LocalDateTime expEnd;
+    private List<GetProductInfoProductTypeResponse> productTypeList;
 
-    public static GetProductInfoResponse from(Product product){
-        return GetProductInfoResponse.builder()
-                .id(product.getId())
-                .images(product.getImages())
-                .name(product.getName())
-                .thumbnail(product.getThumbnail())
-                .cityDeliveryFee(product.getCityDeliveryFee())
-                .mtDeliveryFee(product.getMtDeliveryFee())
-                .expStart(product.getExpStart())
-                .expEnd(product.getExpEnd())
-                .build();
-    }
 }
 
 
