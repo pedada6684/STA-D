f394e3b0842dceef7b565c42997d48cb72de3251
이태경 <korea9300@naver.com>
2024-05-13 13:08:54 +0900

    feat: 광고 재생시 kafka 이벤트 발행

--- stad-alert/src/main/java/com/klpc/stadalert/domain/alert/controller/event/AdvertsStartEvent.java
+++ stad-alert/src/main/java/com/klpc/stadalert/domain/alert/controller/event/AdvertsStartEvent.java
@@ -3,9 +3,11 @@ package com.klpc.stadalert.domain.alert.controller.event;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
-public class AdvertStartEvent {
-    private Long advertId;
+public class AdvertsStartEvent {
     private Long userId;
+    private List<Long> advertIdList;
 }
