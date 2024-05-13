ce29c3a4c4c667086ea145d241ae77e97b344fef
이태경 <korea9300@naver.com>
2024-05-13 11:18:45 +0900

    feat: AlertController.onContentStartEvent 추가

--- stad-alert/src/main/java/com/klpc/stadalert/global/config/KafkaConsumerConfig.java
+++ stad-alert/src/main/java/com/klpc/stadalert/global/config/KafkaConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.klpc.stadalert.global.config;
 
+import com.klpc.stadalert.domain.alert.controller.event.ContentStartEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,12 +23,12 @@ public class KafkaConsumerConfig {
 	@Value("${spring.kafka.bootstrap-servers}")
 	private String bootstrapServers;
 
-//	@Bean
-//	ConcurrentKafkaListenerContainerFactory<String, GetUserInfoResponse> testEventKafkaListenerContainerFactory() {
-//		ConcurrentKafkaListenerContainerFactory<String, GetUserInfoResponse> factory = new ConcurrentKafkaListenerContainerFactory<>();
-//		factory.setConsumerFactory(consumerFactory(GetUserInfoResponse.class));
-//		return factory;
-//	}
+	@Bean
+	ConcurrentKafkaListenerContainerFactory<String, ContentStartEvent> ContentStartEventKafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, ContentStartEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory(ContentStartEvent.class));
+		return factory;
+	}
 
 	public <V> ConsumerFactory<String, V> consumerFactory(
 		Class<V> valueType
