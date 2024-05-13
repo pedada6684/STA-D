685d752c0dedfb7ddada298258dcdec787c969a5
이태경 <korea9300@naver.com>
2024-05-13 11:18:45 +0900

    feat: AlertController.onContentStartEvent 추가
    Former-commit-id: 7d9669edc4ee651bc4703e5b10ba3506c1ab3d07

--- stad-alert/src/main/java/com/klpc/stadalert/domain/alert/controller/AlertController.java
+++ stad-alert/src/main/java/com/klpc/stadalert/domain/alert/controller/AlertController.java
@@ -1,15 +1,14 @@
 package com.klpc.stadalert.domain.alert.controller;
 
-import com.klpc.stadalert.domain.connect.controller.request.ConnectRequest;
-import com.klpc.stadalert.domain.connect.controller.request.QrLoginRequest;
-import com.klpc.stadalert.domain.connect.service.ConnectService;
+import com.klpc.stadalert.domain.alert.controller.event.ContentStartEvent;
+import com.klpc.stadalert.domain.contents.entity.ContentDetail;
+import com.klpc.stadalert.domain.contents.service.ContentsService;
+import com.klpc.stadalert.global.response.ErrorCode;
+import com.klpc.stadalert.global.response.exception.CustomException;
 import com.klpc.stadalert.global.service.SseEmitters;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -21,17 +20,21 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class AlertController {
 
 	final SseEmitters sseEmitters;
-	final ConnectService connectService;
+	final ContentsService contentsService;
 
-	@PostMapping()
-	public ResponseEntity<SseEmitter> connect(@RequestBody ConnectRequest request) {
-		log.info("CreateConnectRequest: "+ request);
-		SseEmitter emitter = sseEmitters.subscribe(request.toCommand());
-		return ResponseEntity.ok(emitter);
+	@KafkaListener(topics = "content-start", groupId = "contents-group", containerFactory = "ContentStartEventKafkaListenerContainerFactory")
+	public void onContentStartEvent(ContentStartEvent event) {
+		log.info("ContentStartEvent: " + event);
+		ContentDetail contentDetail = contentsService.findContentsDetail(event.getContentId());
+		SseEmitter emitter = sseEmitters.emit(
+				"app" + event.getUserId(),
+				contentDetail,
+				"Content Start"
+		);
+		if (emitter == null){
+			throw new CustomException(ErrorCode.EMIT_NOT_FOUND);
+		}
 	}
 
-//	@KafkaListener(topics = "ad-start", groupId = "ad-group", containerFactory = "testEventKafkaListenerContainerFactory")
-//	public void onFollowEvent(GetUserInfoResponse event) {
-//		log.info("kafka-test: {}", event);
-//	}
+
 }
