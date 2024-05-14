cb408767709e1dc2b74949387d67d2e35b7c9439
이서윤 <s2yunn@gmail.com>
2024-05-14 10:47:11 +0900

    feat: 이전 검색 결과 초기화 렌더링
    Former-commit-id: 1c28dc932c3695b3efadd4717ad5e5973d879da3

--- stad-web/src/components/Keyboard/CustomKeyboard.tsx
+++ stad-web/src/components/Keyboard/CustomKeyboard.tsx
@@ -1,18 +1,21 @@
 import Keyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
 import { koreanLayout } from "./koreanLayout";
-import { Dispatch, SetStateAction, useState } from "react";
+import { useEffect, useState } from "react";
 import hangul from "hangul-js";
+import styles from "./Keyboard.module.css";
 interface keyboardProps {
   text: string;
-  setText: Dispatch<SetStateAction<string>>;
+  setText: (newText: string) => void;
   onEnter: () => void;
+  isVisible: boolean;
 }
 
 export default function CustomKeyboard({
   text,
   setText,
   onEnter,
+  isVisible,
 }: keyboardProps) {
   const [layoutName, setLayoutName] = useState<string>("default");
   const onKeyPress = (key: string) => {
@@ -24,18 +27,25 @@ export default function CustomKeyboard({
     } else if (key === "{enterNum}" || key === "{enterText}") {
       onEnter();
       console.log(text);
-    } else if (key === "{dot}") {
-      setText((prev: string) => prev + ".");
-    } else if (key === "{space}") {
-      setText((prev: any) => prev + " ");
+    } else if (key === "{dot}" || key === "{space}") {
+      setText(text + (key === "{dot}" ? "." : " "));
     } else {
       //  한글 라이브러리 사용해서 자모음 합치기
       setText(hangul.assemble(hangul.disassemble(text + key)));
     }
   };
 
+  useEffect(() => {
+    // isVisible이 변경될 때마다 로그를 찍어봄
+    console.log("Keyboard visibility changed: ", isVisible);
+  }, [isVisible]);
+
   return (
-    <div>
+    <div
+      className={`${styles.keyboardContainer} ${
+        isVisible ? styles.nonActive : styles.active
+      }`}
+    >
       <Keyboard
         layoutName={layoutName}
         layout={{ ...koreanLayout }}
