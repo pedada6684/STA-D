8770b1db31595f0a5fc036a681a3b1051651284b
이서윤 <s2yunn@gmail.com>
2024-05-14 10:00:15 +0900

    feat: 가상키보드로 검색 구현

--- stad-web/src/pages/Search/TVSearchInput.tsx
+++ stad-web/src/pages/Search/TVSearchInput.tsx
@@ -20,13 +20,10 @@ export default function TVSearchInput({
   onSearch,
   onSearchAttempted,
 }: SearchInputProps) {
-  const accessToken = useSelector(
-    (state: RootState) => state.token.accessToken
-  );
   const handleSearchClick = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     onSearchAttempted(); // 검색 시도 상태 업데이트
-    const response = await GetSearch(value, accessToken);
+    const response = await GetSearch(value);
     console.log(response);
     // 검색 결과가 있으면 그 결과를 사용하고, 없으면 null로 설정
     onSearch(response ? response : null);
@@ -40,7 +37,7 @@ export default function TVSearchInput({
 
   const executeSearch = async () => {
     onSearchAttempted();
-    const response = await GetSearch(value, accessToken);
+    const response = await GetSearch(value);
     onSearch(response ? response : []);
   };
 
