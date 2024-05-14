655849fb70331c1a9c344c991db4a7ea459cff66
이서윤 <s2yunn@gmail.com>
2024-05-14 13:12:53 +0900

    feat: 유저 찜한 영상 목록 컴포넌트 추가

---
+++ stad-web/src/components/Carousel/SaveCarousel.tsx
@@ -0,0 +1,69 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "react-query";
+import { CarouselVideoProps } from "./MainCarousel";
+import { GetSaveWatching } from "./CarouselApI";
+import Loading from "../Loading";
+import { SmallNextArrow, SmallPrevArrow } from "../Arrow/Arrow";
+import Slider from "react-slick";
+
+export default function SaveCarousel() {
+  const tvUserId = useSelector((state: RootState) => state.tvUser.userId);
+  const navigate = useNavigate();
+  const { data: saveData, isLoading } = useQuery<CarouselVideoProps[]>(
+    "saveList",
+    () => GetSaveWatching(tvUserId)
+  );
+  if (isLoading)
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  let setting = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    speed: 500,
+    appendDots: (dots: any) => (
+      <div
+        style={{
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <ul> {dots} </ul>
+      </div>
+    ),
+    dotsClass: "dots_custom",
+    prevArrow: <SmallPrevArrow />,
+    nextArrow: <SmallNextArrow />,
+  };
+  return (
+    <div className="v-container">
+      <div className="v-title">최근 시청중인 컨텐츠</div>
+      <div className="thumbnail-container">
+        {saveData && saveData.length > 0 ? (
+          <Slider {...setting}>
+            {saveData?.map((data, index) => (
+              <div
+                className="s-vid-container"
+                key={index}
+                style={{ position: "relative", transition: "all 0.3s" }}
+                onClick={() => navigate(`/tv/${data.detailId}`)}
+              >
+                <img src={data.thumbnailUrl} alt="비디오 썸네일" />
+                <div className="vidTitle">{data.title}</div>
+              </div>
+            ))}
+          </Slider>
+        ) : (
+          <div className="no-content">현재 시청 중인 영상이 없습니다.</div>
+        )}
+      </div>
+    </div>
+  );
+}
