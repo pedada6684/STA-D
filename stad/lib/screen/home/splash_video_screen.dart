724343101c8428afe7a768447c1281792d62b3c5
김은지 <eungg0214@naver.com>
2024-05-16 10:05:41 +0900

    feat: 현재 인기 콘텐츠
    Former-commit-id: e247e89b21be4a06825414fbda5ee38c39157afc

--- stad/lib/screen/home/splash_video_screen.dart
+++ stad/lib/screen/home/splash_video_screen.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:stad/constant/animation/vertical_split_route.dart';
+import 'package:provider/provider.dart';
 import 'package:stad/constant/colors.dart';
-import 'package:stad/screen/home/onboarding_screen.dart';
+import 'package:stad/providers/user_provider.dart';
 import 'package:video_player/video_player.dart';
 
 class SplashVideoScreen extends StatefulWidget {
@@ -26,14 +26,18 @@ class _SplashVideoScreenState extends State<SplashVideoScreen> {
         setState(() {});
         _controller.play();
         _controller.setLooping(false);
-        
+
         _timer = Timer(Duration(seconds: 3), () {
           _controller.pause(); // 3초 후 비디오를 일시정지
-          // Navigator.pushReplacement(
-          //     context,
-          //     TVSwitchOnRoute(page: OnboardingScreen()) // 여기에 새 애니메이션 라우트를 적용
-          // );
-          context.go('/onboarding');
+
+          final userProvider =
+              Provider.of<UserProvider>(context, listen: false);
+
+          if (userProvider.isLoggedIn) {
+            context.go('/home');
+          } else {
+            context.go('/onboarding');
+          }
         });
       });
   }
@@ -45,12 +49,12 @@ class _SplashVideoScreenState extends State<SplashVideoScreen> {
       body: Center(
         child: _controller.value.isInitialized
             ? Stack(
-          children: <Widget>[
-            Positioned.fill(
-              child: VideoPlayer(_controller),
-            ),
-          ],
-        )
+                children: <Widget>[
+                  Positioned.fill(
+                    child: VideoPlayer(_controller),
+                  ),
+                ],
+              )
             : Container(),
       ),
     );
