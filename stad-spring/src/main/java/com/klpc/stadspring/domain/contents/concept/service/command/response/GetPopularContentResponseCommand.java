2213069777e0110411f1398e947289c3b6efe99b
최은희 <gilukji226@naver.com>
2024-05-16 14:31:10 +0900

    feat : 인기 콘텐츠 dto 수정
    Former-commit-id: 9968fea241163250adf9fa78aa10947b81c0364a

--- stad-spring/src/main/java/com/klpc/stadspring/domain/contents/concept/service/command/response/GetPopularContentResponseCommand.java
+++ stad-spring/src/main/java/com/klpc/stadspring/domain/contents/concept/service/command/response/GetPopularContentResponseCommand.java
@@ -8,12 +8,24 @@ import lombok.Getter;
 @Builder
 public class GetPopularContentResponseCommand {
     private String title;
+    private String audienceAge;
+    private String cast;
+    private String creator;
+    private String description;
+    private String playtime;
+    private String releaseYear;
     private String thumbnailUrl;
     private Long conceptId;
 
     public static GetPopularContentResponseCommand from(ContentConcept contentConcept) {
         return GetPopularContentResponseCommand.builder()
                 .title(contentConcept.getTitle())
+                .audienceAge(contentConcept.getAudienceAge())
+                .cast(contentConcept.getCast())
+                .creator(contentConcept.getCreator())
+                .description(contentConcept.getDescription())
+                .playtime(contentConcept.getPlaytime())
+                .releaseYear(contentConcept.getReleaseYear())
                 .thumbnailUrl(contentConcept.getThumbnailUrl())
                 .conceptId(contentConcept.getId())
                 .build();
