75c845179d37c2c42cd4bf120576acad4bde8b72
이태경 <korea9300@naver.com>
2024-05-18 01:05:09 +0900

    feat: Notification Entity로 SSE 이벤트 DB보관

--- stad-alert/src/main/java/com/klpc/stadalert/domain/connect/entity/Notification.java
+++ stad-alert/src/main/java/com/klpc/stadalert/domain/connect/entity/Notification.java
@@ -2,15 +2,14 @@ package com.klpc.stadalert.domain.connect.entity;
 
 import com.klpc.stadalert.global.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 @Inheritance(strategy = InheritanceType.JOINED)
-@Entity
 @Table(name = "notifications")
+@Entity
 @Getter
-@ToString
-public abstract class Notification extends BaseEntity {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,12 +19,13 @@ public abstract class Notification extends BaseEntity {
 	@Enumerated(EnumType.STRING)
 	private NotificationStatus status;
 
-	void updateNotificationInfo(
-		String receiver,
-		NotificationStatus status
-	) {
-		this.receiver = receiver;
-		this.status = status;
+	public static Notification createNewNotifcation(
+			String receiver
+	){
+		Notification notification = new Notification();
+		notification.receiver = receiver;
+		notification.status = NotificationStatus.UNREAD;
+		return notification;
 	}
 
 	public void readNotification() {
