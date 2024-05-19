2f36f5d86c8e340cef736e6b30d35f45e21549da
이서윤 <s2yunn@gmail.com>
2024-05-19 17:10:19 +0900

    fix: Donut Chart Error Fix
    tmp2
    
    tmp3
    
    fix: Donut Chart Rendering fix
    
    fix: 렌더링 최소화
    
    fix: pie chart
    
    fix: pie chart
    
    fix: set all chart rendering timer
    
    fix: 타이머 시간 추가
    
    fix: undefined 에러 픽스
    
    fix: 타이머 시간 연장
    
    fix: 조건부 렌더링 추가
    
    fix: 조건부 렌더링 바 + 차트
    
    fix: 조건부 렌더링 제거 타이머 재설정
    
    fix: 디버깅

--- stad-web/src/pages/AdManagement/All/AllRevenue.tsx
+++ stad-web/src/pages/AdManagement/All/AllRevenue.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import BarChart from "../../../components/AdManagement/BarChart";
 import Frame from "../../../components/AdManagement/Frame";
 import GetRevenue from "../../../components/AdManagement/GetRevenue";
@@ -5,6 +6,13 @@ import PieChart from "../../../components/AdManagement/PieChart";
 import styles from "../AdManagement.module.css";
 import { AdvertIdProps } from "./AllExposure";
 export default function AllRevenue({ advertId }: AdvertIdProps) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // 차트 로드 순서 제어
+    }, 20000); // 300ms 지연
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className={`${styles.etcContainer}`}>
       <div className={`${styles.chartWrapper}`}>
