e290ba0573d9ed0f26be8b904ac9eb3890c333fe
이태경 <korea9300@naver.com>
2024-05-19 16:02:49 +0900

    test: Redis Cache Test api 요청 추가

--- stad-stream/src/main/java/com/klpc/stadstream/domain/contents/detail/controller/ContentDetailController.java
+++ stad-stream/src/main/java/com/klpc/stadstream/domain/contents/detail/controller/ContentDetailController.java
@@ -44,4 +44,23 @@ public class ContentDetailController {
         }
         return resourceRegionResponseEntity;
     }
+
+    @GetMapping("test/{userId}/{detailId}")
+    @Operation(summary = "콘텐츠 스트리밍", description = "콘텐츠 스트리밍")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "콘텐츠 스트리밍 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 형식"),
+            @ApiResponse(responseCode = "500", description = "내부 서버 오류")
+    })
+    ResponseEntity<ResourceRegion> testStreamingPublicVideo(@RequestHeader HttpHeaders httpHeaders, @PathVariable Long userId, @PathVariable Long detailId){
+        log.info("TestStreamingPublicVideo: userId: "+userId + " contents: "+detailId);
+        ResponseEntity<ResourceRegion> resourceRegionResponseEntity = detailService.testStreamingPublicVideo(httpHeaders, detailId);
+        //알림서비스 연결
+        boolean isFirstRequest = redisService.isFirstContentStreamingRequest(userId, detailId);
+        if (isFirstRequest){
+            kafkaTemplate.send("content-start", new ContentStartEvnet(userId, detailId));
+            redisService.increaseContentPlayCount(detailId);
+        }
+        return resourceRegionResponseEntity;
+    }
 }
